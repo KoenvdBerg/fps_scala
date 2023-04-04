86d95152523e1104c206ea9f697c394f4f5261f0
KoenvdBerg <koen-vandenberg@live.nl>
2023-04-04 21:09:13 +0200

    implemented foldLeft using my mind only

---
+++ academy/src/main/scala/chapter02.scala
@@ -0,0 +1,76 @@
+
+object test {
+  def abs(n: Int): Int = {
+    if (n < 0) then -n
+    else n
+  }
+
+  def formatResult(name: String, x: Int, f: Int => Int): String = {
+    val msg = s"The $name of ${x} is ${f(x)}"
+    msg
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(formatResult("abs", -42, abs))
+  }
+
+  def factorial(n: Int): Int = {
+    def go(n: Int, acc: Int): Int = {
+      if n <= 0 then acc
+      else go(n-1, acc*n)
+    }
+    go(n, 1)
+  }
+
+  // 2.1
+  def fibbonaci(n: Int): Int = {
+    def go(a: Int, b: Int, n: Int): Int = {
+      if n == 0 then a
+      else
+        go(b, a+b, n-1)
+    }
+    go(0, 1, n)
+  }
+
+  def findFirst[A](as: Array[A], p: A => Boolean): Int = {
+    def loop(n: Int): Int = {
+      if n >= as.length then -1
+      else if p(as(n)) then n
+      else loop(n + 1)
+    }
+    loop(0)
+  }
+
+  // 2.2
+  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+    def loop(n: Int): Boolean = {
+      if n+1 >= as.length then true
+      else if !ordered(as(n), as(n + 1)) then false
+      else
+        loop(n+1)
+    }
+    loop(0)
+  }
+
+  def partial1[A, B, C](a: A, f: (A, B) => C): B => C = {
+    (b: B) => f(a,b)
+  }
+
+  // 2.3
+  def curry[A, B, C](f: (A, B) => C): A => B => C = {
+    (a: A) => (b: B) => f(a,b)
+  }
+
+  // 2.4
+  def uncurry[A, B, C](f: A => B => C): (A, B) => C = {
+    (a: A, b: B) => f(a)(b)
+  }
+
+  // 2.5
+    def compose[A, B, C](f: B => C, g: A => B): A => C = {
+    (a: A) => f(g(a))
+  }
+  //val f = compose((y: Int) => y * 2, (x: Int) => x + 1)
+}
+
+
--- academy/src/main/scala/chapter03.scala
+++ academy/src/main/scala/chapter03.scala
@@ -1,78 +1,8 @@
 
-object test {
-  def abs(n: Int): Int = {
-    if (n < 0) then -n
-    else n
-  }
-
-  def formatResult(name: String, x: Int, f: Int => Int): String = {
-    val msg = s"The $name of ${x} is ${f(x)}"
-    msg
-  }
-
-  def main(args: Array[String]): Unit = {
-    println(formatResult("abs", -42, abs))
-  }
-
-  def factorial(n: Int): Int = {
-    def go(n: Int, acc: Int): Int = {
-      if n <= 0 then acc
-      else go(n-1, acc*n)
-    }
-    go(n, 1)
-  }
-
-  // 2.1
-  def fibbonaci(n: Int): Int = {
-    def go(a: Int, b: Int, n: Int): Int = {
-      if n == 0 then a
-      else
-        go(b, a+b, n-1)
-    }
-    go(0, 1, n)
-  }
-
-  def findFirst[A](as: Array[A], p: A => Boolean): Int = {
-    def loop(n: Int): Int = {
-      if n >= as.length then -1
-      else if p(as(n)) then n
-      else loop(n + 1)
-    }
-    loop(0)
-  }
-
-  // 2.2
-  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
-    def loop(n: Int): Boolean = {
-      if n+1 >= as.length then true
-      else if !ordered(as(n), as(n + 1)) then false
-      else
-        loop(n+1)
-    }
-    loop(0)
-  }
-
-  def partial1[A, B, C](a: A, f: (A, B) => C): B => C = {
-    (b: B) => f(a,b)
-  }
-
-  // 2.3
-  def curry[A, B, C](f: (A, B) => C): A => B => C = {
-    (a: A) => (b: B) => f(a,b)
-  }
-
-  // 2.4
-  def uncurry[A, B, C](f: A => B => C): (A, B) => C = {
-    (a: A, b: B) => f(a)(b)
-  }
-
-  // 2.5
-    def compose[A, B, C](f: B => C, g: A => B): A => C = {
-    (a: A) => f(g(a))
-  }
-  //val f = compose((y: Int) => y * 2, (x: Int) => x + 1)
-}
-
+// 3.1
+// case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
+// 3
+// this is the one because it's earlier in the cases then the next one, which would also work.
 
 object L {
   sealed trait List[+A]
@@ -173,15 +103,22 @@ object L {
   def length[A](as: List[A]): Int =
     foldRight(as, 0)((_, y) => 1 + y)
 
-//  scala > val x = L.List("foo", "bar", "bac", "baz", "foobar", "joost")
-//  val x: L.List[String] = Cons(foo, Cons(bar, Cons(bac, Cons(baz, Cons(foobar, Cons(joost, Nil))))))
-//
-//  scala > L.length(x)
-//  val res1: Int = 6
+  //  scala > val x = L.List("foo", "bar", "bac", "baz", "foobar", "joost")
+  //  val x: L.List[String] = Cons(foo, Cons(bar, Cons(bac, Cons(baz, Cons(foobar, Cons(joost, Nil))))))
+  //
+  //  scala > L.length(x)
+  //  val res1: Int = 6
+
+  // 3.10
+  def foldLeft[A, B](as: List[A], z: B)(f: (B, A) => B): B = as match
+    case Cons(x, xs) => foldLeft(xs, f(z, x))(f)
+    case Nil => z
 }
 
-// 3.1
-// case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
-// 3
-// this is the one because it's earlier in the cases then the next one, which would also work.
 
+
+@main def runthis(): Unit = {
+  val x = L.List(1,2,3,4)
+  val res = L.foldLeft(x, 1)((a, b) => a + a*b)
+  println(res)
+}
