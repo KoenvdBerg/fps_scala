66951a3d8b52dfb2e201122081c6d40d3a1bfed6
KoenvdBerg <koen-vandenberg@live.nl>
2023-04-09 22:48:44 +0200

    idea for day06

---
+++ aoc2018/src/main/scala/day06.scala
@@ -0,0 +1,8 @@
+class day06 {
+  // IDEA: make grid of Point(x, y) based on input max and min border
+  // then determine for each point to which point it belongs using manhatten distance
+  //  - if belongs --> place in Map / data structure
+  //  - if tied, ignore
+  // then filter out locs that have points at border coords
+  // then select largest from remaining
+}
