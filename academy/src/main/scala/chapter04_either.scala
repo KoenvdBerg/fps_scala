a277597cad1e21d0e9d6d6f22f91fb619d98b01a
Koen <kvandenberg@MacBook-Pro-van-Koen.local>
2023-04-12 14:08:24 +0200

    finished chapter04

---
+++ academy/src/main/scala/chapter04_either.scala
@@ -0,0 +1,106 @@
+sealed trait KEither[+E, +A]:
+  def map[B](f: A => B): KEither[E, B] = this match
+    case KRight(a) => KRight(f(a))
+    case KLeft(e) => KLeft(e)
+  def flatMap[EE >: E, B](f: A => KEither[EE, B]): KEither[EE, B] = this match
+    case KRight(a) => f(a)
+    case KLeft(e) => KLeft(e)
+  def orElse[EE >: E, B >: A](b: => KEither[EE, B]): KEither[EE, B] = this match
+    case KRight(a) => KRight(a)
+    case KLeft(_) => b
+  def map2[EE >: E, B, C](b: KEither[EE, B])(f: (A, B) => C): KEither[EE, C] =
+    // this.flatMap(aa => b.map(bb => f(aa, bb)))
+    for {
+      aa <- this
+      bb <- b
+    } yield f(aa, bb)
+case class KLeft[+E](value: E) extends KEither[E, Nothing]
+case class KRight[+A](value: A) extends KEither[Nothing, A]
+
+
+object KEither {
+  def mean(xs: IndexedSeq[Double]): KEither[String, Double] =
+    if xs.isEmpty then KLeft("mean of empty list")
+    else KRight(xs.sum / xs.length)
+
+  def safeDiv(x: Int, y: Int): KEither[Exception, Int] =
+    Try(x / y)
+
+  def Try[A](a: => A): KEither[Exception, A] =
+    try KRight(a)
+    catch { case e: Exception => KLeft(e) }
+
+  // 4.7
+  def traverse[E, A, B](as: List[A])(f: A => KEither[E, B]): KEither[E, List[B]] = as match
+    case h :: t => f(h).flatMap(hh => traverse(t)(f).map(hh :: _))
+    case Nil => KRight(Nil)
+
+  def sequence[E, A](es: List[KEither[E, A]]): KEither[E, List[A]] =
+    traverse(es)((f: KEither[E, A]) => f)
+
+
+  case class Person(name: Name, age: Age)
+  sealed class Name(val value: String)
+  sealed class Age(val value: Int)
+
+
+  // 4.8
+  def mkName(name: String): KEither[String, Name] =
+    if name == "" | name == null then KLeft("Name is empty")
+    else KRight(new Name(name))
+
+  def mkAge(age: Int): KEither[String, Age] =
+    if age < 0 then KLeft("Age is out of range")
+    else KRight(new Age(age))
+
+  def mkPerson(name: String, age: Int): KEither[String, Person] =
+    mkName(name).map2(mkAge(age))(Person(_, _))
+
+  // To improve this, I would make the error type of Either of type List, so that all the
+  // errors that are found can be appended to that list.
+}
+
+
+@main def C04Either(): Unit =
+  val x = Vector(1.1,2.2,4,5,20)
+  println(KEither.mean(x))
+  println(KEither.mean(Vector()))
+  println(KEither.safeDiv(10,5))
+  println(KEither.safeDiv(10,0))
+
+  println("### 4.6 ###")
+  val tr = KRight(33)
+  val tl = KLeft("Multiplication impossible")
+  println(tr.map(_ * 33))
+  println(tl.map(o => 0 * 33))
+
+  println(tr.orElse(KRight(100)))
+  println(tl.orElse(KRight(100)))
+
+  println(tr.flatMap(o => KRight(o * 100)))
+  println(tr.flatMap(o => KLeft(KEither.Try(o / 0))))
+
+  val mres = KLeft("skdfks").map2(KRight(100))((a: Int, b: Int) => a * b)
+  val mres2 = KRight(999).map2(KRight(100))((a: Int, b: Int) => a * b)
+  println(mres)
+  println(mres2)
+
+  println("### 4.7 ###")
+  val z = List(1,2,3,4,5)
+  val res1 = KEither.traverse(z)(f => KEither.Try(10 / f))
+  println(res1)
+
+  val u = List(1,2,0,3,4,5)
+  val res2 = KEither.traverse(u)(f => KEither.Try(100 / f))
+  println(res2)
+
+  val s = List(KRight(1), KRight(4), KRight(9))
+  val s2 = List(KRight(1), KRight(4), KRight(9), KLeft(2))
+  println(KEither.sequence(s))
+  println(KEither.sequence(s2))
+
+  println("### 4.8 ###")
+  println(KEither.mkName("bob"))
+  println(KEither.mkName(""))
+  println(KEither.mkPerson("bob", 40))
+  println(KEither.mkPerson("", -10))
--- academy/src/main/scala/chapter04_option.scala
+++ academy/src/main/scala/chapter04_option.scala
@@ -57,53 +57,58 @@ case object KNone extends KOption[Nothing]
 // 4.2
 import math.*
 import scala.util.Try
-def mean(xs: Seq[Double]): KOption[Double] =
-  if (xs.isEmpty) KNone
-  else KSome(xs.sum / xs.length)
 
-def variance(xs: Seq[Double]): KOption[Double] =
-  val m = mean(xs)
-  val y = m.map(mm => xs.map(x => math.pow(x - mm, 2)))
-  y.flatMap(v => mean(v))
+object KOption {
+  def mean(xs: Seq[Double]): KOption[Double] =
+    if (xs.isEmpty) KNone
+    else KSome(xs.sum / xs.length)
 
-def lift[A, B](f: A => B): KOption[A] => KOption[B] = _.map(f)
+  def variance(xs: Seq[Double]): KOption[Double] =
+    val m = mean(xs)
+    val y = m.map(mm => xs.map(x => math.pow(x - mm, 2)))
+    y.flatMap(v => mean(v))
 
-// 4.3
-def map2[A, B, C](a: KOption[A], b: KOption[B])(f: (A, B) => C): KOption[C] =
-  a.flatMap(aa => b.map(bb => f(aa, bb)))
+  def lift[A, B](f: A => B): KOption[A] => KOption[B] = _.map(f)
 
-def map22[A, B, C](a: KOption[A], b: KOption[B])(f: (A, B) => C): KOption[C] =
-  for {
-    aa <- a
-    bb <- b
-  } yield f(aa, bb)
+  // 4.3
+  def map2[A, B, C](a: KOption[A], b: KOption[B])(f: (A, B) => C): KOption[C] =
+    a.flatMap(aa => b.map(bb => f(aa, bb)))
 
-// 4.4
-def sequence[A](a: List[KOption[A]]): KOption[List[A]] =
-  def go(aa: List[KOption[A]], acc: List[A]): KOption[List[A]] = aa match
-    case h :: t => h match
-      case KSome(v) => go(t, v :: acc)
-      case KNone => KNone
-    case Nil => KSome(acc.reverse)
-  go(a, Nil)
+  def map22[A, B, C](a: KOption[A], b: KOption[B])(f: (A, B) => C): KOption[C] =
+    for {
+      aa <- a
+      bb <- b
+    } yield f(aa, bb)
 
-def Try[A](a: => A): KOption[A] =
-  try KSome(a)
-  catch {case e: Exception => KNone}
-def parseInts(a: List[String]): KOption[List[Int]] =
-  sequence(a.map(i => Try(i.toInt)))
+  // 4.4
+  def sequence[A](a: List[KOption[A]]): KOption[List[A]] =
+    def go(aa: List[KOption[A]], acc: List[A]): KOption[List[A]] = aa match
+      case h :: t => h match
+        case KSome(v) => go(t, v :: acc)
+        case KNone => KNone
+      case Nil => KSome(acc.reverse)
+    go(a, Nil)
 
-def parseInt(a: String): KOption[Int] =
-  Try(a.toInt)
+  def Try[A](a: => A): KOption[A] =
+    try KSome(a)
+    catch {case e: Exception => KNone}
+  def parseInts(a: List[String]): KOption[List[Int]] =
+    sequence(a.map(i => Try(i.toInt)))
 
-// 4.5
-def traverse[A, B](a: List[A])(f: A => KOption[B]): KOption[List[B]] = a match
-//  case h :: t => f(h).flatMap(hh => traverse(t)(f).map(hh :: _))
-  case h :: t => map22(f(h), traverse(t)(f))(_ :: _)
-  case Nil => KSome(Nil)
+  def parseInt(a: String): KOption[Int] =
+    Try(a.toInt)
 
-def sequence2[A](a: List[KOption[A]]): KOption[List[A]] =
-  traverse(a)((o: KOption[A]) => o)
+  // 4.5
+  def traverse[A, B](a: List[A])(f: A => KOption[B]): KOption[List[B]] = a match
+    //  case h :: t => f(h).flatMap(hh => traverse(t)(f).map(hh :: _))
+    case h :: t => map22(f(h), traverse(t)(f))(_ :: _)
+    case Nil => KSome(Nil)
+
+  def sequence2[A](a: List[KOption[A]]): KOption[List[A]] =
+    traverse(a)((o: KOption[A]) => o)
+
+
+}
 
 
 
@@ -126,30 +131,30 @@ def sequence2[A](a: List[KOption[A]]): KOption[List[A]] =
 
   println("### 4.2 ###")
   val test = List(1.1,2,3,4,5)
-  println(mean(test))
+  println(KOption.mean(test))
 
-  println(variance(test))
-  println(variance(Nil))
+  println(KOption.variance(test))
+  println(KOption.variance(Nil))
 
   println("### 4.3 ###")
-  println(map2(KSome(1), KSome(4))((x: Int, y: Int) => x + y))
-  println(map2(KSome("Hello"), KSome(" World"))((x, y) => x + y))
-  println(map2(KSome("Hello"), KNone)((x, y) => x + y))
+  println(KOption.map2(KSome(1), KSome(4))((x: Int, y: Int) => x + y))
+  println(KOption.map2(KSome("Hello"), KSome(" World"))((x, y) => x + y))
+  println(KOption.map2(KSome("Hello"), KNone)((x, y) => x + y))
 
   println("### 4.4 ###")
-  println(sequence(List(KSome(2), KSome(4))))
-  println(sequence(List(KSome(2), KSome(4), KNone)))
+  println(KOption.sequence(List(KSome(2), KSome(4))))
+  println(KOption.sequence(List(KSome(2), KSome(4), KNone)))
 
-  println(parseInts(List("1", "2", "-11")))
-  println(parseInts(List("1", "2", "-11", "jksdhfg")))
+  println(KOption.parseInts(List("1", "2", "-11")))
+  println(KOption.parseInts(List("1", "2", "-11", "jksdhfg")))
 
   println("### 4.5 ###")
   val y = List("1", "12", "3", "skojf")
   val y2 = List("1", "12", "3", "88")
-  val res = traverse(y)(parseInt)
-  val res2 = traverse(y2)(parseInt)
+  val res = KOption.traverse(y)(KOption.parseInt)
+  val res2 = KOption.traverse(y2)(KOption.parseInt)
   println(res)
   println(res2)
 
-  println(sequence2(List(KSome(1), KSome(2))))
-  println(sequence2(List(KSome(1), KSome(2), KNone)))
+  println(KOption.sequence2(List(KSome(1), KSome(2))))
+  println(KOption.sequence2(List(KSome(1), KSome(2), KNone)))
