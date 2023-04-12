882d1c6e7a91a5be1a5f90029910df6f4dcc58e1
KoenvdBerg <koen-vandenberg@live.nl>
2023-04-12 20:29:29 +0200

    ideas for day18

---
+++ aoc2018/src/main/scala/day18.scala
@@ -0,0 +1,9 @@
+class day18 {
+  // IDEA:
+  // - store the field in a 1d Seq
+  // - use the neighbor function to determine neihbors for each cell
+  // - memoize the neighbor function
+  // - implement logic
+  // - count # and | and multiply to get result
+
+}
