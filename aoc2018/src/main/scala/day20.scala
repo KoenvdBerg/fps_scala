6cf0767fd12f1cf05aad48c6f6413dcf729637fb
Koen <kvandenberg@MacBook-Pro-van-Koen.local>
2023-04-14 13:19:42 +0200

    day 20

--- aoc2018/src/main/scala/day20.scala
+++ aoc2018/src/main/scala/day20.scala
@@ -31,41 +31,11 @@ object day20 extends App:
 
     go(r)
 
+  // NOTE THAT 1 example doens't work 30 and not 18
+
   private val res = longestRoute(input).length - 2
   private val answer1 = res
   println(s"Answer day $day part 1: ${answer1} [${System.currentTimeMillis - start1}ms]")
-
-  // NOTE!!! Experiment here with this function, rewriting it to find the first part between ( and )
-//  def findFirstInstance[A](as: Array[A], f: (A, A) => Boolean): (Int, Int) = {
-//    def loop(n: Int): (Int, Int) = {
-//      if n + 1 >= as.length then (-1, -1)
-//      else if f(as(n), as(n + 1)) then (n, n + 1)
-//      else
-//        loop(n + 1)
-//    }
-//
-//    loop(0)
-//  }
-
-  def stringFrom(s: String)(f: Char => Boolean): Option[String] =
-    def loop(n: Int): Option[String] = {
-      if n >= s.length then None
-      else if f(s(n)) then Some(s.slice(n, s.length))
-      else
-        loop(n + 1)
-    }
-    loop(0)
-
-
-  def stringFromTo(s: String)(from: Char => Boolean)(to: Char => Boolean): String =
-    def go(ins: String, n: Int, start: Int)(f: Char => Boolean): String = {
-      if n >= ins.length then ""
-      else if f(ins(n)) then
-        if start == 0 then ins.slice(0, n+1) else ins.slice(n, ins.length)
-      else
-        go(ins, n + 1, start)(f)
-    }
-    go(go(s, 0, 0)(to), 0, 1)(from)
 
   private val start2: Long =
     System.currentTimeMillis
