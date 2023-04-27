56af0f603f95ae57d64bb9fd7497201de1bd59bf
Koen <kvandenberg@MacBook-Pro-van-Koen.local>
2023-04-27 13:41:40 +0200

    working pathfinding function

--- aoc2018/src/main/scala/day15.scala
+++ aoc2018/src/main/scala/day15.scala
@@ -43,33 +43,28 @@ object day15 extends App:
         Point(x, y + 1)
       )
 
-    def add(p2: Point): Point = Point(x + p2.x, y + p2.y)
+    def +(p2: Point): Point = Point(x + p2.x, y + p2.y)
 
-  case class Path(len: Int, path: Vector[Point])
+    // breadth first search algorithm
+    def -->(target: Point, obstacles: Set[Point]): Option[Vector[Point]] =
 
-  // Flooding algorithm
-  def pathFinding(source: Point, target: Point, obstacles: Set[Point]): Option[Path] =
+      val seen: mutable.Set[Point] = obstacles.to(mutable.Set)
+      var queue: mutable.Queue[Vector[Point]] = mutable.Queue[Vector[Point]](Vector(this))
 
-    def go(current: Point, visited: Set[Point], path: Path): Option[Path] =
-      if current == target then Some(Path(path.len + 1, (current +: path.path).reverse))
-      else
-        val next: Set[Point] = current
-          .adjacent                  // compute the adjacent Points
-          .diff(visited)             // filter out the points already visited
-        if next.isEmpty then None    // if next is empty then this path is a dead end, thus None
+      def loop(): Option[Vector[Point]] =
+        if queue.isEmpty then None      // exit condition when no path can be found
         else
-          // here the final path is computed by appending each current path to the path trajectory, increasing path
-          // length with 1. Also, the current path is added to the list of visited, and thus cannot be reached anymore.
-          val allPaths: Set[Path] = next
-            .flatMap(cc => go(cc, Set(current) ++ visited, Path(path.len + 1, current +: path.path)))
-
-          // here we select the shortest path to final destination
-          if allPaths.isEmpty then None  // if no viable path found then no path is possible to target, thus None
+          val thisPath = queue.dequeue()
+          val thisPoint = thisPath.head
+          seen += thisPoint
+          if thisPoint == target then Some(thisPath)  // exit condition when target is reached
           else
-            Some(allPaths.minBy(p => p._1))  // select path with shortest path length
-
-    go(source, obstacles, Path(0, Vector.empty))
-
+            val next: Set[Point] = thisPoint
+              .adjacent             // compute the adjacent Points
+              .diff(seen)           // filter out the points already visited
+            queue = queue ++ next.map(n => n +: thisPath)
+            loop()
+      loop()
 
   sealed trait Infantry
   case object Goblin extends Infantry
@@ -84,18 +79,18 @@ object day15 extends App:
     def nonSelfTargets(army: Vector[Soldier]): Vector[Soldier] =
       army.filter(s => s.unit != this.unit)
 
-    def findClosestTarget(targets: Vector[Soldier], obstacles: Set[Point]): Option[Path] =
-      val allPaths = targets.map(_.loc).flatMap(t => pathFinding(this.loc, t, obstacles))
+    def findClosestTarget(targets: Vector[Soldier], obstacles: Set[Point]): Option[Vector[Point]] =
+      val allPaths: Vector[Vector[Point]] = targets.map(_.loc).flatMap(t => this.loc --> (t, obstacles))
       if allPaths.isEmpty then None
-      else Some(allPaths.minBy(_.len))
+      else Some(allPaths.minBy(_.length))
 
     def move(army: Vector[Soldier], obstacles: Set[Point]): Soldier =
-      val targets = nonSelfTargets(army)
+      val targets: Vector[Soldier] = nonSelfTargets(army)
       if enemyAdjacent(targets) then this    // skip expensive computation of pathFinding
       else
-        val path = findClosestTarget(targets, obstacles)
+        val path: Option[Vector[Point]] = findClosestTarget(targets, obstacles)
         path match
-          case Some(p) => Soldier(this.unit, p.path(1), this.hp, this.pwr)
+          case Some(p) => Soldier(this.unit, p(p.length-2), this.hp, this.pwr)
           case None    => this
 
 
@@ -134,9 +129,36 @@ object day15 extends App:
 
     round(army.sortBy(p => (p.loc.x, p.loc.y)))
 
+  def print2dGrid(army: Vector[Soldier], obstacles: Set[Point]): Unit =
+    val map: Set[Point] = obstacles | army.map(_.loc).toSet
+    val xMax: Int = map.maxBy(_.x).x
+    val yMax: Int = map.maxBy(_.y).y
+
+    val grid: Vector[Point] = (for {
+      x <- Range(0, xMax+1).toList
+      y <- Range(0, yMax+1).toList
+    } yield Point(x, y)).toVector
+
+    def go(grid: Vector[Point], n: Int = 0): Unit = grid match
+      case c +: t =>
+        val s: Option[Soldier] = army.find(_.loc == c)
+        val w: Option[Point] = obstacles.find(_ == c)
+        if n % (xMax+1) == 0 then println() else ()
+        (s, w) match
+          case (None, None) => print(" ")
+          case (None, Some(_)) => print("#")
+          case (Some(s), None) => s.unit match
+            case Elve => print("E")
+            case Goblin => print("G")
+        go(t, n + 1)
+      case Vector() => println()
+    go(grid)
+
+// TODO: then update the print function to only print using obstacles
 
   println(infantry)
-  println(simulate(infantry, obstacles))
+//  println(simulate(infantry, obstacles))
+  print2dGrid(infantry, obstacles)
   private val answer1 = None
   println(s"Answer day $day part 1: ${answer1} [${System.currentTimeMillis - start1}ms]")
 
@@ -156,3 +178,50 @@ object day15 extends App:
 //case class Goblin(hp: Int, pwr: Int, loc: Point) extends Infantry
 //
 //case class Elve(hp: Int, pwr: Int, loc: Point) extends Infantry
+//def pathFinding2(source: Point, target: Point, obstacles: Set[Point]): Option[Path] =
+//
+//  val queue: mutable.Queue[Path] = mutable.Queue[Path](Path(0, Vector(source)))
+//  val seen: mutable.Set[Point] = obstacles.to(mutable.Set)
+//
+//  def loop(): Option[Path] =
+//    if queue.isEmpty then None
+//    else
+//      val path: Path = queue.dequeue()
+//      val thisPoint: Point = path.path.last
+//      if thisPoint == target then Some(path)
+//      else
+//        val next
+//
+//        for {
+//          cc <- thisPoint.adjacent.diff(seen)
+//        } do {
+//          seen += cc
+//          queue.enqueue(Path(path.len + 1, thisPoint +: path.path))
+//          loop()
+//        }
+//        None
+//
+//  loop()
+// Flooding algorithm
+//def pathFinding(source: Point, target: Point, obstacles: Set[Point]): Option[Path] =
+//
+//  def go(current: Point, visited: Set[Point], path: Path): Option[Path] =
+//    if current == target then Some(Path(path.len + 1, (current +: path.path).reverse))
+//    else
+//      println(path)
+//      val next: Set[Point] = current
+//        .adjacent // compute the adjacent Points
+//        .diff(visited) // filter out the points already visited
+//      if next.isEmpty then None // if next is empty then this path is a dead end, thus None
+//      else
+//        // here the final path is computed by appending each current path to the path trajectory, increasing path
+//        // length with 1. Also, the current path is added to the list of visited, and thus cannot be reached anymore.
+//        val allPaths: Set[Path] = next
+//          .flatMap(cc => go(cc, Set(current) ++ visited, Path(path.len + 1, current +: path.path)))
+//
+//        // here we select the shortest path to final destination
+//        if allPaths.isEmpty then None // if no viable path found then no path is possible to target, thus None
+//        else
+//          Some(allPaths.minBy(p => p._1)) // select path with shortest path length
+//
+//  go(source, obstacles, Path(0, Vector.empty))
