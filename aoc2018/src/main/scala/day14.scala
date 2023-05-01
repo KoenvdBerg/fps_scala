d0f06635f2ec35cbbf77b4d4a2675d3002a89a07
Koen <kvandenberg@MacBook-Pro-van-Koen.local>
2023-05-01 14:08:58 +0200

    processed comments Marco

--- aoc2018/src/main/scala/day14.scala
+++ aoc2018/src/main/scala/day14.scala
@@ -26,18 +26,18 @@ object day14 extends App:
   def simulateRecepiCreation(board: Vector[Int], maxN: Int, leapSize: Int, players: Map[Int, Int]): Vector[Int] =
     if board.length - leapSize == maxN then board.takeRight(leapSize)
     else
-      val playerVals: Vector[Int] = players.map(p => board(p._2)).toVector
+      val playerVals: Vector[Int] = players.values.map(board(_)).toVector
       val recipe: Int = playerVals.sum
       val toAdd: Vector[Int] = if recipe >= 10 then splitInteger(recipe) else Vector(recipe)
       val newBoard: Vector[Int] = board ++: toAdd
-      val updatedPlayers: Map[Int, Int] = players
-        .map(p => (p._1, (p._2 + playerVals(p._1) + 1) % newBoard.length))
+      val updatedPlayers: Map[Int, Int] = players.view
+        .map(p => (p._1, (p._2 + playerVals(p._1) + 1) % newBoard.length)).toMap
       simulateRecepiCreation(newBoard, maxN, leapSize, updatedPlayers)
 
 
   private val res: Vector[Int] = simulateRecepiCreation(Vector(3, 7), input, 10, Map(0 -> 0, 1 -> 1))
   private val answer1: String = res.mkString("")
-  println(s"Answer day $day part 1: ${answer1} [${System.currentTimeMillis - start1}ms]")
+  println(s"Answer day $day part 1: ${answer1} should be: 7121102535 [${System.currentTimeMillis - start1}ms]")
 
 
   def simulateRecepiCreation2(board: Vector[Int], toFind: Vector[Int], leapSize: Int, players: Map[Int, Int], l: Int = 2): Int =
@@ -57,4 +57,4 @@ object day14 extends App:
 
   private val toFind = splitInteger(input).take(5)  // just looking for 5 digits as this is already a proper signature
   private val answer2 = simulateRecepiCreation2(Vector(3, 7), toFind, toFind.length, Map(0 -> 0, 1 -> 1))
-  println(s"Answer day $day part 2: ${answer2} [${System.currentTimeMillis - start2}ms]")
+  println(s"Answer day $day part 2: ${answer2} should be: 20236441 [${System.currentTimeMillis - start2}ms]")
