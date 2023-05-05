9cb930822916100d8d24ab491b2dd59f4672b9fa
Koen <kvandenberg@MacBook-Pro-van-Koen.local>
2023-05-05 13:51:07 +0200

    added docs and cleaner code

--- aoc2018/src/main/scala/day17.scala
+++ aoc2018/src/main/scala/day17.scala
@@ -4,6 +4,37 @@ import aoc2018.Grid2D.Point
 import scala.collection.mutable
 import scala.collection.immutable.Queue
 
+/**
+ *
+ * PART 01:
+ *
+ * The difficult part was to get the movement right. I originally started trying to reuse the breath first search
+ * algorithm from day15, but the performance on the final input was an absolute disaster. So I changed plans to
+ * work with just 1 coordinate, no path tracking. The revelation was to make a autonomous function that could take
+ * care of pouring water to the sides. It scans into a direction (left or right) and then returns the movement
+ * possible at that end: Still (for still water), Down (for waterfall). Then by pattern matching on the result of
+ * the left and right end of a split, the continuation points in the simulation could be determined. Also, based on
+ * that pattern match, water is added to the water collection; ~ when both ends are Still, | otherwise. In case of
+ * Still on both ends, then make sure that the ground Set is updated as well (i.e. still water is now also an obstacle).
+ *
+ * Another minor issue was the arranging of the offset of x-coordinate. I didn't want to deal with coordinates around
+ * 500, so I subtracted that from every x-coordinate.
+ *
+ * Another issue was that in my specific final input, the water left the leftmost part of the render at the very last
+ * reservoir before reaching the bottom. To work with this, I changed the offset and shifted everything one x-coord
+ * to the right.
+ *
+ * The final issue was that the source was 8 tiles higher than the topmost clay. I needed to make sure that the source
+ * was at the level of the topmost clay to prevent counting these 8 water tiles. I solved this by computing an yMin
+ * and set the source to that y-coordinate.
+ *
+ *
+ * PART 02:
+ * due to the way part 1 was implemented, the solution to part 2 was a plain and simple count of the character ~.
+ *
+ *
+ */
+
 object day17 extends App:
 
   private val day: String =
@@ -38,77 +69,74 @@ object day17 extends App:
   def waterfall(start: Point, clay: Vector[(Point, Char)]): Vector[(Point, Char)] =
 
     import Movement.*
-    val ground: mutable.Set[Point] = clay.map(_._1).to(mutable.Set)
-    val maxY: Int = ground.maxBy(_.y).y + 1
+    val maxY: Int = clay.map(_._1).maxBy(_.y).y + 1  // the y-coordinate that the water should not pass to prevent infinity
 
-    def pourDown(c: Point): Point =
-      val next: Point = c.adjacentDown.head
-      next
+    def pourDown(c: Point): Point = Point(c.x, c.y + 1)
 
-    def pourSide(c: Point): ((Movement, Point), (Movement, Point)) =
+    def pourSide(c: Point, ground: Set[Point]): ((Movement, Point), (Movement, Point)) =
+
       def go(q: Queue[Point], dir: String): (Movement, Point) =
         val (p, rem): (Point, Queue[Point]) = q.dequeue
-        val next: Option[Point] = p.adjacentSides(dir).diff(ground).headOption
-        next match
-          case Some(pp) =>
-            if ground.contains(Point(pp.x, pp.y + 1)) then go(rem.enqueue(pp), dir) else (Down, pp)
-          case None     => (Still, p)
+        val nextP: Point = dir match
+          case "left"  => Point(p.x - 1, p.y)
+          case "right" => Point(p.x + 1, p.y)
+        if ground.contains(nextP) then (Still, p)  // the water at this side is blocked and thus movement becomes Still
+        else
+          if ground.contains(Point(nextP.x, nextP.y + 1)) then go(rem.enqueue(nextP), dir)  // the water cannot go down thus go further in dir
+          else (Down, nextP)  // the water can go down here, so this sides new movement is Down
 
       val left: (Movement, Point) = go(Queue[Point](c), "left")
       val right: (Movement, Point) = go(Queue[Point](c), "right")
       (left, right)
 
-    def updateWaterXRange(water: Vector[(Point, Char)], left: Point, right: Point, aqua: Char): Vector[(Point, Char)] =
+    def updateWaterXRange(water: Vector[(Point, Char)], left: Point, right: Point)(aqua: Char): Vector[(Point, Char)] =
       val toChange: Vector[Point] = Range(left.x, right.x + 1).map(f => Point(f, left.y)).toVector
       toChange.map((_, aqua)) ++ water.filterNot(a => toChange.contains(a._1))
 
-    def updateGroundXRange(left: Point, right: Point): Unit =
-      val toChange: Vector[Point] = Range(left.x, right.x + 1).map(f => Point(f, left.y)).toVector
-      toChange.foreach(p => ground.add(p))
+    def updateGroundXRange(ground: Set[Point], left: Point, right: Point): Set[Point] =
+      val toAdd: Set[Point] = Range(left.x, right.x + 1).map(f => Point(f, left.y)).toSet
+      ground.union(toAdd)
 
     def updateWaterAtPoint(water: Vector[(Point, Char)], loc: Point): Vector[(Point, Char)] =
       ((loc, '|') +: water).distinct
 
-    def simulate(queue: Queue[Point], water: Vector[(Point, Char)] = Vector.empty): Vector[(Point, Char)] =
+    def simulate(queue: Queue[Point], ground: Set[Point], water: Vector[(Point, Char)] = Vector.empty): Vector[(Point, Char)] =
       if queue.isEmpty then water
       else
         val (p, rem): (Point, Queue[Point]) = queue.distinct.dequeue
 
-        // determine move
-        val nextMove: Movement = if p.y >= maxY then Halt else if p.adjacentDown.diff(ground).isEmpty then Side else Down
+        // loading bar print, comment to disable
+        if p.y % 500 == 0 then println(s"Reached depth: ${p.y} / $maxY")
 
-        if p.y % 50 == 0 then println(s"Reached depth away from maxDepth: ${p.y} / $maxY")
+        // determine next movement
+        val nextMove: Movement = if p.y >= maxY then Halt else if ground.contains(pourDown(p)) then Side else Down
 
-
-        // run water
+        // move water
         nextMove match
-          case Down => simulate(rem.enqueue(pourDown(p)), updateWaterAtPoint(water, p))
+          case Down => simulate(rem.enqueue(pourDown(p)), ground, updateWaterAtPoint(water, p))
           case Side =>
-            val sides: ((Movement, Point), (Movement, Point)) = pourSide(p)
-            (sides._1._1, sides._2._1) match
-              case (Down, Down)    =>
-                val nextWater: Vector[(Point, Char)] = updateWaterXRange(water, sides._1._2, sides._2._2, '|')
-                simulate(rem.enqueueAll(List(sides._1._2, sides._2._2)), nextWater)
-              case (Still, Down)   =>
-                val nextWater: Vector[(Point, Char)] = updateWaterXRange(water, sides._1._2, sides._2._2, '|')
-                simulate(rem.enqueue(sides._2._2), nextWater)
-              case (Down, Still)   =>
-                val nextWater: Vector[(Point, Char)] = updateWaterXRange(water, sides._1._2, sides._2._2, '|')
-                simulate(rem.enqueue(sides._1._2), nextWater)
+            val sides: ((Movement, Point), (Movement, Point)) = pourSide(p, ground)
+            val nextWater: Char => Vector[(Point, Char)] = updateWaterXRange(water, sides._1._2, sides._2._2)
+            (sides._1._1, sides._2._1) match  // match the left and right movement of the water after splitting
+              case (Down, Down)    =>  simulate(rem.enqueueAll(List(sides._1._2, sides._2._2)), ground, nextWater('|'))
+              case (Still, Down)   =>  simulate(rem.enqueue(sides._2._2), ground, nextWater('|'))
+              case (Down, Still)   =>  simulate(rem.enqueue(sides._1._2), ground, nextWater('|'))
               case (Still, Still)  =>
-                val nextWater: Vector[(Point, Char)] = updateWaterXRange(water, sides._1._2, sides._2._2, '~')
-                updateGroundXRange(sides._1._2, sides._2._2)
-                simulate(rem.enqueue(Point(p.x, p.y-1)), nextWater)
+                // both ends blocked, add still water to ground, then go 1 up and continue from there
+                val nextGround: Set[Point] = updateGroundXRange(ground, sides._1._2, sides._2._2)
+                simulate(rem.enqueue(Point(p.x, p.y-1)), nextGround, nextWater('~'))
               case _ => sys.error("cannot simulate further from here")
-          case Halt => simulate(rem, water)
+          case Halt => simulate(rem, ground, water)
           case _ => sys.error("NO MOVEMENT POSSIBLE")
 
 
-    simulate(Queue[Point](Point(start.x, start.y + 1)))
+    simulate(Queue[Point](Point(start.x, start.y + 1)), clay.map(_._1).toSet)
 
 
   val start: Point = clay.filter(_._2 == '+').head._1
   val res1 = waterfall(start, clay)
+
+  // print the filled clay ground, (un)comment to disable/enable
   // Point.print2dGrid(res1 ++ clay)
 
   private val answer1: Int = res1.length
