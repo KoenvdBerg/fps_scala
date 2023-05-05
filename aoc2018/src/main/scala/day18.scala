3b82fd4411613c9772c42fb5ca06b7ca11f46c1d
Koen <kvandenberg@MacBook-Pro-van-Koen.local>
2023-05-05 17:04:21 +0200

    added docs to day18

--- aoc2018/src/main/scala/day18.scala
+++ aoc2018/src/main/scala/day18.scala
@@ -1,6 +1,29 @@
 import scala.io.*
 import math.*
+import scala.annotation.tailrec
 import scala.collection.mutable
+
+/**
+ *
+ * PART 01:
+ *
+ * The difficulty of this puzzle is to get the neighbor function right. I happened to already have a function
+ * that determines for each index in a flat 1d Vector what it's neighbours are based on the rowWidth of a grid.
+ * I updated that function to work with 8 neighbours instead of 4 and viola, this puzzle was done.
+ *
+ * To make the result neat, I also included a function that can print the area as it's getting updated, so that
+ * you can see the progress that is made at the construction site.
+ *
+ * On top of that, I memoized the neighbour function to make it quicker.
+ *
+ * PART 02:
+ *
+ * Similar to problem day12, the pattern stabilizes at some point. For my input that happened at minute 489. Then I
+ * made a map and used that map to find the final result. See the code and explanation written down below in the code
+ * for part 2.
+ *
+ */
+
 
 object day18 extends App:
 
@@ -38,29 +61,32 @@ object day18 extends App:
 
     memoize(get).apply(i)
 
-  def sim(in: Vector[Char], exit: Int, min: Int = 0): Vector[Char] =
+  @tailrec
+  def sim(area: Vector[Char], exit: Int, min: Int = 0): Vector[Char] =
 
-    def go(area: Vector[Char], next: Vector[Char], tile: Int = 0): Vector[Char] =
+    @tailrec
+    def go(next: Vector[Char], tile: Int = 0): Vector[Char] =
         if tile >= area.length then next
         else
           val nbrs: Vector[Char] = neighbours8(tile).map(area(_))
           area(tile) match
-          case '.' if nbrs.count(_ == '|') >= 3 => go(area, next.patch(tile, Vector('|'), 1), tile + 1)
-          case '|' if nbrs.count(_ == '#') >= 3 => go(area, next.patch(tile, Vector('#'), 1), tile + 1)
-          case '#' if nbrs.count(_ == '#') < 1 || nbrs.count(_ == '|') < 1 => go(area, next.patch(tile, Vector('.'), 1), tile + 1)
-          case _ => go(area, next, tile + 1)
+          case '.' if nbrs.count(_ == '|') >= 3 => go(next.patch(tile, Vector('|'), 1), tile + 1)
+          case '|' if nbrs.count(_ == '#') >= 3 => go(next.patch(tile, Vector('#'), 1), tile + 1)
+          case '#' if nbrs.count(_ == '#') < 1 || nbrs.count(_ == '|') < 1 => go(next.patch(tile, Vector('.'), 1), tile + 1)
+          case _ => go(next, tile + 1)
 
     println(min)
-    printArea(in)
-    if min >= exit then in
+    printArea(area)
+    if min >= exit then area
     else
-      val next: Vector[Char] = go(in, in)
+      val next: Vector[Char] = go(area)
       sim(next, exit, min + 1)
 
+  @tailrec
   def printArea(area: Vector[Char]): Unit =
-    val (p, next) = area.splitAt(rowSize)
-    if p.isEmpty then ()
+    if area.isEmpty then ()
     else
+      val (p, next) = area.splitAt(rowSize)
       p.foreach(print(_))
       println()
       printArea(next)
