4a4cec15bcf693f6f20424616479e5937bcb3503
Koen <kvandenberg@MacBook-Pro-van-Koen.local>
2023-05-26 16:03:44 +0200

    cleanup for day1,2,3

--- aoc2018/src/main/scala/day03.scala
+++ aoc2018/src/main/scala/day03.scala
@@ -1,5 +1,22 @@
 import scala.io.*
 
+/**
+ *
+ * PART 1:
+ *
+ * The difficult part is parsing in the input. This code parses the input to claims, and then computes for every position
+ * how many unique claim IDs are present on that position. It counts the positions with 2 or more claims and that's
+ * the answer to this part.
+ *
+ * PART 2:
+ *
+ * The solution is to take all the positions on which 2 or more claims are present, and then to take all their IDs.
+ * The difference between the set of those IDs with all the IDs in the input is the answer. This works, because
+ * the problem states that 1 claim goes completely without overlap.
+ *
+ */
+
+
 object day03 extends App:
 
   private val day: String =
@@ -8,39 +25,41 @@ object day03 extends App:
   private val start1: Long =
     System.currentTimeMillis
 
-  private val input: List[String] =
+  private val claims: List[Claim] =
+
+    def parseClaim(s: String): List[Claim] = s match
+      case s"#$id @ $xstart,$ystart: ${width}x$length" =>
+        (for {
+          x <- Range(xstart.toInt, xstart.toInt + width.toInt)
+          y <- Range(ystart.toInt, ystart.toInt + length.toInt)
+        } yield Claim(x, y, id.toInt)).toList
+      case _ => sys.error("BOOM!!!")
+
     Source
       .fromResource(s"day$day.txt")
       .getLines
       .toList
+      .flatMap(parseClaim)
 
   case class Claim(x: Int, y: Int, id: Int)
 
-  def computeClaim(cs: String): List[Claim] =
-    val t = cs.split(" ")
-    val id = t(0).substring(1).toInt
-    val xstart = t(2).split(",")(0).toInt + 1
-    val ystart = t(2).split(",")(1).replace(":", "").toInt + 1
-    val xend = t(3).split("x")(0).toInt + xstart
-    val yend = t(3).split("x")(1).toInt + ystart
-
-    (for {
-      x <- Range(xstart, xend)
-      y <- Range(ystart, yend)
-    } yield Claim(x, y, id)).toList
-
-  val claims = input.flatMap(computeClaim(_))
-  private val answer1: Int = claims.groupBy(x => (x.x, x.y)).map((_, y) => y.size).count(_ > 1)
+  private val answer1: Int = claims
+    .groupBy((x: Claim) => (x.x, x.y))  // group by positions for each claim
+    .map((_, y) => y.size)              // find per position how many times it's occupied
+    .count(_ > 1)                       // count claims that are occupied more than once
   println(s"Answer day $day part 1: ${answer1} [${System.currentTimeMillis - start1}ms]")
 
 
   private val start2: Long =
     System.currentTimeMillis
 
-  def getAllIds(in: List[String]): List[Int] =
-    in.map(_.split(" ")(0).substring(1).toInt)
+  val allIds: Set[Int] = claims.map(_.id).toSet
 
-  val idswithmultipleclaims = claims.groupBy(x => (x.x, x.y)).filter((_, y) => y.size > 1).map((_, y) => y).flatten.map(_.id).toSet
-  val allids = getAllIds(input).toSet
-  private val answer2 = allids.diff(idswithmultipleclaims)
-  println(s"Answer day $day part 2: ${answer2} [${System.currentTimeMillis - start2}ms]")
+  val idswithmultipleclaims: Set[Int] = claims
+    .groupBy((x: Claim) => (x.x, x.y))          // group by positions for each claim
+    .filter((_, y: List[Claim]) => y.size > 1)  // select every position with two or more claims
+    .flatMap((_, y: List[Claim]) => y)          // make list of claims
+    .map(_.id)                                  // take all the ids for positions that have two or more claims
+    .toSet
+  private val answer2 = allIds.diff(idswithmultipleclaims)  // the result is the 1 claim that has no overlap
+  println(s"Answer day $day part 2: ${answer2.head} [${System.currentTimeMillis - start2}ms]")
