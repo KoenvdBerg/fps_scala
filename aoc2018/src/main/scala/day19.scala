a25d18f28abe1ea695de1832926132fb97a267f0
Koen <kvandenberg@MacBook-Pro-van-Koen.local>
2023-05-26 10:20:57 +0200

    added thorough documentation

--- aoc2018/src/main/scala/day19.scala
+++ aoc2018/src/main/scala/day19.scala
@@ -4,7 +4,30 @@ import scala.collection.mutable
 import scala.collection.mutable.Stack
 import scala.util.{Failure, Success, Try}
 
-
+/**
+ *
+ * PART 1
+ *
+ * The idea of this puzzle is to reuse the code from day 16 and update a few properties. In this new puzzle the code
+ * can jump to specific instructions using an extra register that tracks the instruction index. What happens is that
+ * the code at some point will jump to an instruction that doesn't exist and when that happens the code crashes.
+ *
+ * To find this location I've reused the code from day 16. I updated everything to work with immutable data structures
+ * instead of Arrays, to keep things functional and it turned out to be more performant as well. The logic for going
+ * to next and previous instructions (basically a GOTO jump) is added to the code and whenever a instruction is called
+ * that is out of index, then the program crashes. The value left in register 0 when the program crashes is the result
+ * for part 1.
+ *
+ *
+ * PART 2:
+ *
+ * The difficulty of part 2 is that it's an extremely long running program, so you'll have to figure out what's
+ * happening and code a solution that runs quicker. To do this, I ran several tests with the program and eventually
+ * figured out that the program first computes a large number, and then sums all the divisors for that number to the
+ * result. If that's finished, it crashes (I presumed). So the answer is specific to my input, but I think that
+ * the general direction to get to this answer is identical across inputs.
+ *
+ */
 object day19 extends App:
 
   private val day: String =
@@ -63,18 +86,24 @@ object day19 extends App:
 
   def incrementIp(reg: Vector[Int], ip: Int): Vector[Int] = reg.updated(ip, reg(ip) + 1)
 
-  def runProgram(prg: Vector[Operation], ip: Int, n: Int = 0)(maxIts: Int, reg: Vector[Int]): Vector[Int] =
+  /**
+   * The function below will run the input program until the program crashes. A crash is defined as a call to an
+   * instruction pointer that doesn't exist (i.e. IndexError). The `ip` is the index of the register that holds
+   * the instruction number to execute. The `program` holds all the instructions as obtained from the input file in order.
+   */
+  def runProgram(program: Vector[Operation], ip: Int, n: Int = 0)(maxIts: Int, reg: Vector[Int]): Vector[Int] =
     if n >= maxIts && maxIts != -1 then reg
     else
-      val instruction: Try[Operation] = Try(prg(reg(ip)))  // yields Failure if the ip index is not present in the prg
+      // get instruction based on the instruction pointer (ip)
+      val instruction: Try[Operation] = Try(program(reg(ip)))  // yields Failure if the ip index is not present in the program
       instruction match
-        case Failure(_) => {println("HIT") ; reg}
+        case Failure(_) => {println("HIT") ; reg}  // exit condition: instruction was not available thus program has crashed
         case Success(i) =>
-          val next: Vector[Int] = i.run(reg)
-          runProgram(prg, ip, n + 1)(maxIts, incrementIp(next, ip))
+          val next: Vector[Int] = i.run(reg)       // run the instruction and update the registers
+          runProgram(program, ip, n + 1)(maxIts, incrementIp(next, ip))  // increment the instruction pointer
 
-  val prg: (Int, Vector[Int]) => Vector[Int] = runProgram(input._1, input._2)
-  private val answer1: Vector[Int] = prg(-1, Vector(0,0,0,0,0,0))
+  val program: (Int, Vector[Int]) => Vector[Int] = runProgram(input._1, input._2)
+  private val answer1: Vector[Int] = program(-1, Vector(0,0,0,0,0,0))
   println(s"Answer day $day part 1: ${answer1} should be: 1694 [${System.currentTimeMillis - start1}ms]")
 
   private val start2: Long =
@@ -97,7 +126,7 @@ object day19 extends App:
    *   It's summing whatever 10551364 % i == 0
    */
 
-  val limit: Int = prg(1000, Vector(1,0,0,0,0,0)).max
+  val limit: Int = program(1000, Vector(1,0,0,0,0,0)).max
 
   val res2 = Range(1, limit + 1).toVector.filter((i: Int) => limit % i == 0)
   println(res2)
