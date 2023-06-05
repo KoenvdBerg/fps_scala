0fca810065ab95065f53a795a443d25426a36b43
Koen <kvandenberg@MacBook-Pro-van-Koen.local>
2023-06-05 16:34:03 +0200

    working on a bug in day24

--- aoc2018/src/main/scala/day24.scala
+++ aoc2018/src/main/scala/day24.scala
@@ -21,7 +21,9 @@ object day24 extends App:
     def parseGroup(s: String, side: String): Group = s match
       case s"$units units each with $hp hit points $defence with an attack that does $atk $atkType damage at initiative $in" =>
         Group(units.toInt, hp.toInt, parseDefence(defence)._1, parseDefence(defence)._2, atk.toInt, atkType, in.toInt, side)
-      case _ => sys.error("boom")
+      case s"$units units each with $hp hit points with an attack that does $atk $atkType damage at initiative $in" =>
+        Group(units.toInt, hp.toInt, Nil, Nil, atk.toInt, atkType, in.toInt, side)
+      case _ => {println(s);sys.error("boom")}
 
     def processDef(s: String): List[String] =
       s.split(',').toList.map(_.trim)
@@ -54,27 +56,18 @@ object day24 extends App:
 
     def effectivePwr(g: Group): Int = g.atk * g.units
 
-    def determineAtk(g: Group, opponent: Group): Int =
+    def determineDmg(g: Group, opponent: Group): Int =
       if opponent.immune.contains(g.atkType) then 0
       else if opponent.weak.contains(g.atkType) then effectivePwr(g) * 2
       else effectivePwr(g)
 
-    def makeSelection(opponents: Army)(strategy: Group => Int): Army =
-      val strat: Set[(Group, Int)] = opponents.map((g: Group) => (g, strategy(g)))
-      val m: Int = strat.maxBy(_._2)._2
-      strat.filter(_._2 == m).map(_._1)
+    def attack(g: Group, opponent: Group): Group =
+      val dmg: Int = determineDmg(g, opponent)
+      val unitsKilled: Int = dmg / opponent.hp
+      println(s"${g.initiative} ${g.side} attacked with $dmg (${g.atkType}) and killed $unitsKilled from ${opponent.initiative}")
+      opponent.copy(units = opponent.units - unitsKilled)
 
-    def selectTarget(g: Group, opponents: Army): Option[Group] =
-      val strat1: Army = makeSelection(opponents)(determineAtk.curried(g))
-      if strat1.size <= 0 then None
-      else if strat1.size == 1 then Some(strat1.head)
-      else
-        val strat2: Army = makeSelection(strat1)(effectivePwr)
-        if strat2.size == 1 then Some(strat2.head)
-        else
-          Some(makeSelection(strat2)((g: Group) => g.initiative).head)
-
-    def selectionPhase(side: Army, opponents: Army): Vector[(Group, Group)] =
+    def selectionPhase(side: Army, opponents: Army): Vector[(Group, Option[Group])] =
       val groupInOrder: Vector[Group] = side  // sort based on effective power first, then initiative
         .toVector
         .map((g: Group) => (g, (effectivePwr(g), g.initiative)))
@@ -82,30 +75,62 @@ object day24 extends App:
         .map(_._1)
 
       @tailrec
-      def go(orderedArmy: Vector[Group], opps: Army, acc: Vector[(Group, Group)]): Vector[(Group, Group)] = orderedArmy match
+      def go(orderedArmy: Vector[Group], opps: Army, acc: Vector[(Group, Option[Group])]): Vector[(Group, Option[Group])] = orderedArmy match
         case h +: t if opps.nonEmpty =>
-          val selection: Option[Group] = selectTarget(h, opps)
-          selection match
-            case Some(sel) => go(t, opps - sel, (h, sel) +: acc)
-            case None    => go(t, opps, acc)
+          val selection: Group = opps.maxBy((opponent: Group) => (determineDmg(h, opponent), effectivePwr(opponent), opponent.initiative))
+          if determineDmg(h, selection) > 0 then go(t, opps - selection, (h, Some(selection)) +: acc)
+          else go(t, opps, (h, None) +: acc)
+        case h +: t if opps.isEmpty =>  // in case that no opponents are left, remaining ally groups will fight None
+          go(t, opps, (h, None) +: acc)
         case _ => acc
 
       go(groupInOrder, opponents, Vector.empty)
 
-  // TODO: implement attacking phase
-  // TODO: groups attack in decreasing order of initiative, ordered over all sides
-  // TODO: if group dies during attacking phase, remove it can no longer attack
-  // TODO: units killed = dmg / hp_per_unit
 
+    def attackingPhase(selections: Vector[(Group, Option[Group])]): Army =
+      val orderedAttack: Vector[(Group, Option[Group])] = selections
+        .sortBy(_._1.initiative)
+        .reverse                  // group with highest initiative attacks first
 
-  val test = Group.selectionPhase(imm, inf)
-  println(test.mkString("\n\n"))
+      @tailrec
+      def doAttack(s: Vector[(Group, Option[Group])], acc: Army = Set.empty[Group]): Army = s match
+        case h +: t => h match
+          case (g, Some(opp)) if g.units > 0 => // attack is possible because units is positive integer
+            val updatedOpp: Group = attack(g, opp)
+            val oppIndex: Int = t.indexWhere(_._1.initiative == updatedOpp.initiative)
+            if oppIndex <= -1 then
+              // update the opponent in the acc because the opponent itself had already attacked
+              doAttack(t, acc.filter(_.initiative != updatedOpp.initiative) + updatedOpp + g)
+            else
+               // here the opponent still will attack. It's updated so that if the opponent has les than 1 unit, it won't attack
+              doAttack(t.updated(oppIndex, (updatedOpp, t(oppIndex)._2)), acc + g)
+          case (g, _) if g.units <= 0 => doAttack(t, acc)
+          case (g, None)      => doAttack(t, acc + g)
+          case _              => sys.error("no attacker available")
+        case _ => acc
 
-  val test2 = Group.selectionPhase(inf, imm)
-  println("###################")
-  println(test2.mkString("\n\n"))
+      doAttack(orderedAttack).filter(_.units > 0)
 
-  private val answer1 = None
+    @tailrec
+    def fight(imm: Army, inf: Army, n: Int = 0): Army =
+      if imm.isEmpty then inf
+      else if n >= 1 then imm
+      else if inf.isEmpty then imm
+      else
+        //imm.foreach(g => println(s"imm: ${g.units} remaining for ${g.initiative}"))
+        //inf.foreach(g => println(s"inf: ${g.units} remaining for ${g.initiative}"))
+        val selImm: Vector[(Group, Option[Group])] = selectionPhase(imm, inf)
+        val sellInf: Vector[(Group, Option[Group])] = selectionPhase(inf, imm)
+        val fightResult: Army = attackingPhase(selImm ++ sellInf)
+
+        println(fightResult.filter(_.side == "imm").mkString("\n"))
+
+        fight(fightResult.filter(_.side == "imm"), fightResult.filter(_.side == "inf"), n + 1)
+
+  // too low: 14722
+
+  val res1: Army = Group.fight(imm, inf)
+  private val answer1 = res1.map(_.units).sum
   println(s"Answer day $day part 1: ${answer1} [${System.currentTimeMillis - start1}ms]")
 
 
