8f652ba362fedf96284759e827112b22c125ffe1
Koen <kvandenberg@MacBook-Pro-van-Koen.local>
2023-06-20 13:50:28 +0200

    finished chapter13

--- fps_book/src/main/scala/chapter13.scala
+++ fps_book/src/main/scala/chapter13.scala
@@ -1,8 +1,8 @@
 import scala.io.StdIn.readLine
 import chapter11.Monad
-import chapter13_A.IO.{FlatMap, Suspend}
-
+import chapter07.Par
 import scala.annotation.tailrec
+import scala.util.Try
 
 object chapter13_A:
   
@@ -38,24 +38,23 @@ object chapter13_B:
 
     def flatMap[F2[x] >: F[x], B](f: A => Free[F2, B]): Free[F2, B] = FlatMap(this, f)
     def map[B](f: A => B): Free[F, B] = flatMap((a: A) => Return(f(a)))
-
-    @tailrec
-    final def step: Free[F, A] = this match
-      case FlatMap(FlatMap(x, f), g) => x.flatMap(a => f(a).flatMap(g)).step
-      case FlatMap(Return(x), f) => f(x).step
-      case _ => this
-
-
-    def run[F2[x] >: F[x]](using F: Monad[F2]): F2[A] = this.step match
-      case Return(a)  => F.unit(a)
+    
+    def covary[F2[x] >: F[x]]: Free[F2, A] = this
+    
+    // 13.3
+    def run[F2[x] >: F[x]](using F: Monad[F2]): F2[A] = this match
+      case Return(a) => F.unit(a)
       case Suspend(r) => r
       case FlatMap(x, f) => x match
+        case Return(x)  => f(x).run
         case Suspend(r) => F.flatMap(r)(a => f(a).run)
-        case _          => sys.error("Impossible no se puede realizar esta categoria")
+        case FlatMap(xx, ff) => xx.flatMap(a => ff(a).flatMap(f)).run
       
-    
-        
-    
+    // below gives compiler error for stack overflow:
+    //final def step: Free[F, A] = this match
+    //  case FlatMap(FlatMap(x, f), g) => x.flatMap(a => f(a).flatMap(y => g(y).covary[F])).step
+    //  case FlatMap(Return(x), f) => f(x).step
+    //  case _ => this
   object Free:
     // 13.1
     def freeMonad[F[_]]: Monad[[x] =>> Free[F, x]] = new Monad[[x] =>> Free[F, x]]:
@@ -73,6 +72,27 @@ object chapter13_B:
           case Return(a) => f(a).runTrampoline
           case Suspend(r) => f(r()).runTrampoline
           case FlatMap(xx, ff) => xx.flatMap(a => ff(a).flatMap(f)).runTrampoline
+
+object chapter13_C:
+  enum Console[A]:
+    case ReadLine extends Console[Option[String]]
+    case PrintLine(line: String) extends Console[Unit]
+
+    def toPar: Par[A] = this match
+      case ReadLine        => Par.lazyUnit(Try(readLine()).toOption)
+      case PrintLine(line) => Par.lazyUnit(println(line))
+
+    def toThunk: () => A = this match
+      case ReadLine        => () => Try(readLine()).toOption
+      case PrintLine(line) => () => println(line)
+      
+  object Console:
+    import chapter13_B.Free
+    import chapter13_B.Free.*
+    type ConsoleIO[A] = Free[Console, A]
+    
+    def readLn: ConsoleIO[Option[String]] = Suspend(ReadLine)
+    def printLn(line: String): ConsoleIO[Unit] = Suspend(PrintLine(line))
     
 @main def run_chapter13: Unit =
   import chapter13_A.*
@@ -94,63 +114,10 @@ object chapter13_B:
   
   println(g(42).run)
   
-  
-//  val nextC: IO[Unit] = converter.flatMap(a => IO.PrintLine("HAHAHAH"))
-//  val echo: IO[Unit] = IO.ReadLine.flatMap(IO.PrintLine)
-//  val readInt: IO[Int] = IO.ReadLine.map(_.toInt)
-//  val readInts: IO[(Int, Int)] = readInt ** readInt
-//  val multiple: IO[List[String]] = IO.replicateM(3, IO.ReadLine)
-//
-//  val f = IO.forever(x)
-//  println(f.unsafeRun())
-  
-
-//sealed trait IO[A]:
-//  private val self = this
-//
-//  def run: A
-//
-//  def map[AA >: A, B](f: A => B): IO[B] = new IO[B] {
-//    override def run: B = f(self.run)
-//  }
-//
-//  def flatMap[AA >: A, B](f: A => IO[B]): IO[B] = new IO[B] {
-//    override def run: B = f(self.run).run
-//  }
-//
-//object IO:
-//  def unit[A](a: => A): IO[A] = new IO {
-//    override def run: A = a
-//  }
-//
-//  def flatMap[A, B](fa: IO[A])(f: A => IO[B]): IO[B] = fa.flatMap(f)
-//
-//  def ReadLine: IO[String] = IO
-// 
-// NR 2: 
-//case class IO[A](unsafeRun: () => A):
-//  def map[B](f: A => B): IO[B] = IO.map(this)(f)
-//
-//  def flatMap[B](f: A => IO[B]): IO[B] = IO.flatMap(this)(f)
-//
-//  def **[AA >: A, B](iob: IO[B]): IO[(A, B)] = IO.product(this, iob)
-//
-//object IO extends Monad[IO]:
-//  override def unit[A](a: => A): IO[A] = IO(() => a)
-//
-//  override def flatMap[A, B](fa: IO[A])(f: A => IO[B]): IO[B] = IO(() => f(fa.unsafeRun()).unsafeRun())
-//
-//  def PrintLine(msg: String): IO[Unit] = unit(println(msg))
-//
-//  def ReadLine: IO[String] = unit(readLine)
-
-//sealed trait Free[F[_], A]:
-//  def flatMap[B](f: A => Free[F, B]): Free[F, B] = FlatMap(this, f)
-//
-//  def map[B](f: A => B): Free[F, B] = flatMap(f.andThen(Return.apply))
-//
-//case class Return[F[_], A](a: A) extends Free[F, A]
-//
-//case class Suspend[F[_], A](s: F[A]) extends Free[F, A]
-//
-//case class FlatMap[F[_], A, B](s: Free[F, A], f: A => Free[F, B]) extends Free[F, B]
+  // testing the Console object
+  import chapter13_C.Console
+  import chapter13_B.Free
+  val f1: Free[Console, Option[String]] = for {
+    _  <- Console.printLn("I can only interact with the console")
+    ln <- Console.readLn 
+  } yield ln
