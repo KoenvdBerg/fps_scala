42ff8865dd78048956d0ec87a017bbc80af106e5
Koen <kvandenberg@MacBook-Pro-van-Koen.local>
2023-06-26 09:40:25 +0200

    improved using comments JP

--- aoc2022/src/main/scala/day10.scala
+++ aoc2022/src/main/scala/day10.scala
@@ -23,31 +23,47 @@ object day10 extends App:
   private val start1: Long =
     System.currentTimeMillis
 
-  case class Instruction(ex: String, value: Int)
-
   private val input: List[Instruction] =
-
-    def parser(s: String): Instruction = s match
-      case s"noop"    => Instruction("noop", 0)
-      case s"addx $i" => Instruction("addx", i.toInt)
 
     Source
       .fromResource(s"day$day.txt")
       .getLines
       .toList
-      .map(parser)
+      .map(Instruction.parser)
 
-  type Reg = Int
-  type Cycle = Int
-  def readProgram(s: List[(Reg, Cycle)], instruction: Instruction): List[(Reg, Cycle)] =
-    val (x, cycle): (Reg, Cycle) = s.head
-    instruction.ex match
-      case "noop" => (x, cycle + 1) :: s
-      case "addx" => (x + instruction.value, cycle + 2) :: (x, cycle + 1) :: s
-      case _      => sys.error(s"Couldn't process instruction $instruction")
+  enum Instruction: 
+    case Noop
+    case Addx(value: Int)
+    
+  object Instruction:
 
+    type Reg = Int
+    type Cycle = Int
+    type Image = String
+    
+    def parser(s: String): Instruction = s match
+      case s"noop" => Noop
+      case s"addx $i" => Addx(i.toInt)
+    
+    def readProgram(s: List[(Reg, Cycle)], instruction: Instruction): List[(Reg, Cycle)] =
+      val (x, cycle): (Reg, Cycle) = s.head
+      instruction match
+        case Noop        => (x, cycle + 1) :: s
+        case Addx(value) => (x + value, cycle + 2) :: (x, cycle + 1) :: s
+ 
+    def drawProgram(width: Int)(s: (Reg, Image), instruction: Instruction): (Reg, Image) =
 
-  private val interesting: List[Int] = List(20, 60, 100, 140, 180, 220)
+      def nextPixel(i: Int, regX: Int): Char = if i >= regX - 1 && i <= regX + 1 then '#' else '.'
+
+      val (x, image): (Reg, Image) = s
+      val pos: Int = image.length % width
+      instruction match
+        case Noop        => (x, image + nextPixel(pos, x))
+        case Addx(value) => (x + value, image + nextPixel(pos, x) + nextPixel(pos + 1, x))
+
+  import Instruction.*
+  
+  private val interesting: List[Int] = (20 to 220 by 40).toList
   private val res1: List[(Int, Int)] = input.foldLeft(List((1, 1)))(readProgram)
   private val answer1: Int = res1
     .filter((r: (Reg, Cycle)) => interesting.contains(r._2))
@@ -55,25 +71,9 @@ object day10 extends App:
     .sum
   println(s"Answer day $day part 1: ${answer1} [${System.currentTimeMillis - start1}ms]")
 
-
   private val start2: Long =
     System.currentTimeMillis
 
-  private val width: Int = 40
-  type Image = String
-  def drawProgram(s: (Reg, Image), instruction: Instruction): (Reg, Image) =
-    
-    def nextPixel(i: Int, regX: Int): Char = if i >= regX - 1 && i <= regX + 1 then '#' else '.'
-    
-    val (x, image): (Reg, Image) = s
-    val pos: Int = image.length % width
-    instruction.ex match
-      case "noop" => (x, image + nextPixel(pos, x))
-      case "addx" =>
-        (x + instruction.value, image + nextPixel(pos, x) + nextPixel(pos + 1, x))
-      case _ => sys.error(s"Couldn't process instruction $instruction")
-
-
-  private val res2: (Int, String) = input.foldLeft((1, ""))(drawProgram)
-  println(printFlatGrid(res2._2, width)(identity))
+  private val res2: (Int, String) = input.foldLeft((1, ""))(drawProgram(40))
+  println(printFlatGrid(res2._2, 40)(identity))
   println(s"Answer day $day part 2: ^^^^^^^^^ [${System.currentTimeMillis - start2}ms]")
