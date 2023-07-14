ca5a7f1f6e59479ec6ff9bd9252455ecc4a169bd
Koen <kvandenberg@MBP-van-Koen.home>
2023-07-14 18:59:41 +0200

    working on  part02

--- aoc2022/src/main/scala/day20.scala
+++ aoc2022/src/main/scala/day20.scala
@@ -5,7 +5,7 @@ import scala.annotation.tailrec
 import cats.data.State
 
 import scala.collection.immutable.Queue
-import scala.collection.mutable.ListBuffer
+import scala.collection.mutable.ArrayBuffer
 
 /**
  * PART 01:
@@ -27,40 +27,47 @@ object day20 extends App:
   private val start1: Long =
     System.currentTimeMillis
 
-  private val input: Vector[Int] =
+  private val input: Vector[Long] =
     Source
       .fromResource(s"day$day.txt")
       .getLines
-      .map((v: String) => v.toInt)
+      .map((v: String) => v.toLong)
       .toVector
 
-  def getPos(idx: Int, n: Int, s: Int): Int =
-    val dir: Int = (idx + n) % s
+  def getPos(idx: Int, n: Long, s: Int): Int =
+    val dir: Int = ((idx + n) % s).toInt
     if dir <= 0 then s + dir else dir
     
-  def mix(in: Vector[Int]): Vector[Int] = 
+  def mix(in: Vector[Long], times: Int): Vector[Long] = 
     
     val size: Int = in.length - 1
-    val buffer = ListBuffer.from(in.zipWithIndex)
+    val buffer = ArrayBuffer.from(in.zipWithIndex)
     
     @tailrec
-    def go(q: Queue[(Int, Int)]): Unit = 
+    def go(q: Queue[(Long, Int)]): Unit = 
       if q.isEmpty then ()
       else 
         val (n, rem) = q.dequeue
         val idx: Int = buffer.indexOf(n)
-        val elem: (Int, Int) = buffer(idx)
-        
-        buffer.remove(idx)
+        val elem: (Long, Int) = buffer(idx)
+        buffer.remove(idx)  // happy side effect here
         val np: Int = getPos(idx, elem._1, size)
-        buffer.insert(np, elem)
+        buffer.insert(np, elem)  // another happy side effect here
         go(rem)
         
-    go(Queue.from(in.zipWithIndex))
+    def gotimes(t: Int): Unit =
+      if t <= 0 then ()
+      else
+        println(buffer.map(_._1))  
+        go(Queue.from(in.zipWithIndex))
+        gotimes(t - 1)
+      
+    gotimes(times)
+    //go(Queue.from(in.zipWithIndex))
     buffer.toVector.map(_._1)
       
   
-  private val res1: Vector[Int] = mix(input)
+  private val res1: Vector[Long] = mix(input, 1)
   private val zeroIndex: Int = res1.indexWhere(_ == 0)
   private val indices: List[Int] = List(1000, 2000, 3000).map((i: Int) => (i + zeroIndex) % res1.length)
   private val answer1 = indices.map((i: Int) => res1(i)).sum
@@ -69,6 +76,9 @@ object day20 extends App:
 
   private val start2: Long =
     System.currentTimeMillis
-
-  val answer2 = None
+    
+  private val res2: Vector[Long] = mix(input.map(_ * 811589153L), 10)
+  private val zeroIndex2: Int = res1.indexWhere(_ == 0)
+  private val indices2: List[Int] = List(1000, 2000, 3000).map((i: Int) => (i + zeroIndex2) % res2.length)
+  private val answer2 = indices2.map((i: Int) => res2(i)).sum
   println(s"Answer day $day part 2: ${answer2} [${System.currentTimeMillis - start2}ms]")
