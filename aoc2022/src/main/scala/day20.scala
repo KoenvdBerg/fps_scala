95631fbdaaeaae5d7d9fa4a8ff78f337084c7a5d
KoenvdBerg <koen-vandenberg@live.nl>
2023-07-15 14:29:20 +0200

    finished day20

--- aoc2022/src/main/scala/day20.scala
+++ aoc2022/src/main/scala/day20.scala
@@ -2,7 +2,6 @@
 import scala.io.*
 import math.*
 import scala.annotation.tailrec
-import cats.data.State
 
 import scala.collection.immutable.Queue
 import scala.collection.mutable.ArrayBuffer
@@ -10,11 +9,12 @@ import scala.collection.mutable.ArrayBuffer
 /**
  * PART 01:
  *
- * Easy foldRight
+ * First tried solving this without any mutable data structures, but then found that I had to use them to get the
+ * mixing right and also speedy.
  *
  * PART 02:
  *
- * Sort and then take 3 and sum
+ * Updated code for part 1 to be able to run multiple times. See below.
  *
  */
 
@@ -36,34 +36,27 @@ object day20 extends App:
 
   def getPos(idx: Int, n: Long, s: Int): Int =
     val dir: Int = ((idx + n) % s).toInt
-    if dir <= 0 then s + dir else dir
+    if dir < 0 then s + dir else dir
     
   def mix(in: Vector[Long], times: Int): Vector[Long] = 
     
-    val size: Int = in.length - 1
+    val size: Int = in.length - 1  // length here -1 because the element to be mixed is removed first, leading to a size smaller by 1. See below
     val buffer = ArrayBuffer.from(in.zipWithIndex)
-    
-    @tailrec
-    def go(q: Queue[(Long, Int)]): Unit = 
-      if q.isEmpty then ()
-      else 
-        val (n, rem) = q.dequeue
-        val idx: Int = buffer.indexOf(n)
-        val elem: (Long, Int) = buffer(idx)
-        buffer.remove(idx)  // happy side effect here
-        val np: Int = getPos(idx, elem._1, size)
-        buffer.insert(np, elem)  // another happy side effect here
-        go(rem)
-        
+
+    def go(elem: (Long, Int)): Unit =
+      val idx: Int = buffer.indexOf(elem)
+      val toMix: (Long, Int) = buffer(idx)
+      buffer.remove(idx)        // happy side effect here --> first remove element to mix
+      val np: Int = getPos(idx, toMix._1, size)
+      buffer.insert(np, toMix)  // another happy side effect here --> then insert element to mix based on logic of getPos()
+
     def gotimes(t: Int): Unit =
       if t <= 0 then ()
       else
-        println(buffer.map(_._1))  
-        go(Queue.from(in.zipWithIndex))
+        in.zipWithIndex.foldLeft(())((_, elem) => go(elem))
         gotimes(t - 1)
       
     gotimes(times)
-    //go(Queue.from(in.zipWithIndex))
     buffer.toVector.map(_._1)
       
   
@@ -76,9 +69,9 @@ object day20 extends App:
 
   private val start2: Long =
     System.currentTimeMillis
-    
-  private val res2: Vector[Long] = mix(input.map(_ * 811589153L), 10)
-  private val zeroIndex2: Int = res1.indexWhere(_ == 0)
+
+  private val res2: Vector[Long] = mix(input.map(_ * 811589153), 10)
+  private val zeroIndex2: Int = res2.indexWhere(_ == 0)
   private val indices2: List[Int] = List(1000, 2000, 3000).map((i: Int) => (i + zeroIndex2) % res2.length)
   private val answer2 = indices2.map((i: Int) => res2(i)).sum
   println(s"Answer day $day part 2: ${answer2} [${System.currentTimeMillis - start2}ms]")
