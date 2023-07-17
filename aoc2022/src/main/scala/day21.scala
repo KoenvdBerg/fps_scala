f63e467a11af5c606d0a4c198931c9a2fb6081d2
Koen <kvandenberg@MacBook-Pro-van-Koen.local>
2023-07-17 12:05:36 +0200

    finished day21

--- aoc2022/src/main/scala/day21.scala
+++ aoc2022/src/main/scala/day21.scala
@@ -8,24 +8,34 @@ import scala.util.{Failure, Try, Success}
 
 /**
  * PART 01:
+ * 
+ * The main idea for this puzzle is that it basically forms an equation in the form of a tree. I solved this by creating 
+ * the ADT called Equation. Then I loaded the input into this ADT and wrote a solve function that solves the equation. 
  *
  * PART 02:
- * IDEA:
- *
- * instead of using strings, use an ADT that can be interpreted for getting to the right and left
- * hand side of the = sign. Then determine on which one the human has influence. Find the 'fixed' result on which no
- * influence was possible, and then inverse the interpretation of the program that leads to the human through the
- * interpreter.
- *
- * The ADT should look like a tree eg see below
- *
+ * 
  * Fill the tree with both sides of the equation. One side should be solvable, then the other side can be reverse
- * engineered using basic math substitution.
- *
- * TODO: use map for monkeys Map[name, Operation]
- *
- *
- *
+ * engineered using basic math substitution. This has been implemented in solveForX. 
+ * 
+ * In the example case the equation to solve is. This one is not representative for the final input, because it only
+ * has the left-side of the operator (i.e. +-*div) unsolved:
+ * 
+ * (((x-3) * 2) + 4) / 4 = 150
+ * (x-3) * 2) + 4 = 600 
+ * (x-3) * 2 = 596
+ * x-3 = 298
+ * x = 301
+ * 
+ * The tricky part is for when the x is on the other side of the equation, then division and minus change like this: 
+ * 
+ * 10 / x = 2
+ * x = 10 / 2 = 5
+ * 
+ * 10 - x = 5
+ * x = -(5 - 10) = 5
+ * 
+ * This has been incorporated in the code below. 
+ * 
  */
 
 
@@ -54,11 +64,12 @@ object day21 extends App:
       case Div => (a: Long, b: Long) => a * b
 
 
-  def toOp(s: String): Op = s match
-    case "+" => Plus
-    case "-" => Minus
-    case "/" => Div
-    case "*" => Mul
+  object Op: 
+    def toOp(s: String): Op = s match
+      case "+" => Plus
+      case "-" => Minus
+      case "/" => Div
+      case "*" => Mul
 
   private val input: Map[String, String]=
 
@@ -77,106 +88,52 @@ object day21 extends App:
   enum Equation[+A]:
     case Value(a: A)
     case Operation(l: Equation[A], op: Op, r: Equation[A])
-    case Unknown
+    case X
 
   object Equation:
 
     import Op.*
 
     def fromInput(in: Map[String, String], key: String)(part: Int): Equation[Long] = in(key) match
-      case s"$unkown1 $op $unkown2"     => Operation(fromInput(in, unkown1)(part), toOp(op), fromInput(in, unkown2)(part))
-      case s"FLAG $number" if part == 1 => Value(number.toLong)
-      case s"FLAG $_"      if part == 2 => Unknown
+      case s"$x $op $y"     => Operation(fromInput(in, x)(part), toOp(op), fromInput(in, y)(part))
+      case s"FLAG $number" if part == 1 => Value(number.toLong)  // set humn to its value
+      case s"FLAG $_"      if part == 2 => X                     // set humn to X
       case s"$number"                   => Value(number.toLong)
       case _                            => sys.error(s"cannot parse to Expr: $key, $part")
 
-    def interpreter(in: Equation[Long]): Long = in match
-      case Operation(l, op, r) => op.toFunc(interpreter(l), interpreter(r))
+    def solve(in: Equation[Long]): Long = in match
+      case Operation(l, op, r) => op.toFunc(solve(l), solve(r))
       case Value(v) => v
-      case Unknown => sys.error("cannot interpret this tree")
+      case X => sys.error("cannot interpret this tree")
 
-    def invertEquation(in: Equation[Long], inVal: Long): Long = in match
+    @tailrec
+    def solveForX(in: Equation[Long], inVal: Long): Long = in match
       case Operation(l, op, r) =>
-        Try(interpreter(r)) match
-          case Success(value) =>
-            println(s"$inVal $op $value --> ${op.invertFunc(inVal, value)}")
-            invertEquation(l, op.invertFunc(inVal, value))
+        Try(solve(r)) match
+          // first case when right is known e.g. X - 100 = 130  --> 130 + 100 = 230.
+          // use a simple function inversion here to solve for X
+          case Success(value) => solveForX(l, op.invertFunc(inVal, value))
           case Failure(_)     =>
-            println(s"$inVal $op ${interpreter(l)} --> ${op.invertFunc(inVal, interpreter(l))}")
-            invertEquation(r, op.invertFunc(inVal, interpreter(l)))
+            // this case left is unknown, this changes the order for minus and division. 
+            val nextVal: Long = op match
+              case Minus => -(inVal - solve(l))   // e.g. 10 - X = 8 --> X = -(8 - 10) = 2
+              case Div   => inVal / solve(l)      // e.g. 6 / X = 2  --> X = 6 / 2 = 3
+              case _     => op.invertFunc(inVal, solve(l))  // for multiplication and plus this order doesn't matter
+            solveForX(r, nextVal)
       case Value(_) => inVal
-      case Unknown  => inVal
-
-    def solver(left: Equation[Long], right: Equation[Long]): Long =
-      Try(interpreter(left)) match
-        case Success(value) =>
-          println("solving rigth")
-          invertEquation(right, value)
-        case Failure(_)     =>
-          println("solving left")
-          invertEquation(left, interpreter(right))
+      case X  => inVal
 
   private val in1: Equation[Long] = Equation.fromInput(input, "root")(1)
-  private val answer1 = Equation.interpreter(in1)
+  private val answer1 = Equation.solve(in1)
   println(s"Answer day $day part 2: ${answer1} [${System.currentTimeMillis - start1}ms]")
-
 
   private val start2: Long =
     System.currentTimeMillis
 
-  // not it: 8764479278265
-
   private val left: Equation[Long] = Equation.fromInput(input, input("root").split(" ")(0))(2)
   private val right: Equation[Long] = Equation.fromInput(input, input("root").split(" ")(2))(2)
-  private val answer2 = Equation.solver(left, right)
+  private val answer2: Long = Try(Equation.solve(right)) match
+    case Success(toEqual) => Equation.solveForX(left, toEqual)  // solve for left
+    case Failure(_)       => Equation.solveForX(right, Equation.solve(left))  // solve for right
   println(s"Answer day $day part 2: ${answer2} [${System.currentTimeMillis - start2}ms]")
 
-
-
-
-
-
-
-//case class Monkey(name: String, operation: Either[String, String]):
-//
-//  def interpret: Monkey =
-//    val newOp: Try[Either[String, String]] = operation match
-//      case Right(x) => Success(Right(x))
-//      case Left(unknown) => unknown match
-//        case s"$n1+$n2" => Try(Right(s"${n1.toLong + n2.toLong}"))
-//        case s"$n1*$n2" => Try(Right(s"${n1.toLong * n2.toLong}"))
-//        case s"$n1/$n2" => Try(Right(s"${n1.toLong / n2.toLong}"))
-//        case s"$n1-$n2" => Try(Right(s"${n1.toLong - n2.toLong}"))
-//        case _ => sys.error(s"cannot interpret monkey: $unknown")
-//    newOp match
-//      case Failure(_) => this
-//      case Success(v) => this.copy(operation = v)
-//
-//
-//private val input: Vector[Monkey] =
-//
-//  def parse(s: String): Monkey = s match
-//    case s"$name: $unknown1 $op $unknown2" => Monkey(name, Left(unknown1 + op + unknown2))
-//    case s"$name: $number" => Monkey(name, Right(number))
-//    case _ => sys.error(s"Cannot parse monkey: $s")
-//
-//  Source
-//    .fromResource(s"day$day.txt")
-//    .getLines
-//    .toVector
-//    .map(parse)
-//
-//def updateMonkeys(monkeys: Vector[Monkey], m: Monkey): Vector[Monkey] = m.operation match
-//  case Left(_) => monkeys
-//  case Right(known) => monkeys.filterNot(_ == m).foldLeft(Vector.empty)((newms: Vector[Monkey], mm: Monkey) => mm.operation match
-//    case Right(_) => mm +: newms
-//    case Left(unkown) => mm.copy(operation = Left(unkown.replace(m.name, known))) +: newms
-//  )
-//
-//@tailrec
-//def solver(monkeys: Vector[Monkey]): Monkey =
-//  if monkeys.length <= 1 then monkeys.head
-//  else
-//    val updated: Vector[Monkey] = monkeys.foldLeft(monkeys)(updateMonkeys)
-//    val next: Vector[Monkey] = updated.map(_.interpret)
-//    solver(next)
