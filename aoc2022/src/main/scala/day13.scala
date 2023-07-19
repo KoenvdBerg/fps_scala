18a25eed9a82b7395d23ce27f262e0c422d8b1ca
KoenvdBerg <koen-vandenberg@live.nl>
2023-07-19 09:23:49 +0200

    updated using remarks JP

--- aoc2022/src/main/scala/day13.scala
+++ aoc2022/src/main/scala/day13.scala
@@ -59,8 +59,8 @@ object day13 extends App:
     given ordering: Ordering[Packet] with
       override def compare(x: Packet, y: Packet): Int = (x, y) match
         case (N(i1), N(i2))       => i1.compare(i2)
-        case (n@N(_), l@L(_))     => compare(L(List(n)), l)
-        case (l@L(_), n@N(_))     => compare(l, L(List(n)))
+        case (n: N, l: L)     => compare(L(List(n)), l)
+        case (l: L, n: N)     => compare(l, L(List(n)))
         case (L(Nil), L(_ :: _))  => -1 
         case (L(_ :: _), L(Nil))  => 1 
         case (L(Nil), L(Nil))     => 0
@@ -84,6 +84,6 @@ object day13 extends App:
   private val div2: Packet = Packet.parsePacket("[[6]]")  
   
   private val newInput: List[Packet] = (div1 :: div2 :: input).sorted
-  private val answer2: Int = (newInput.indexWhere(_ == div1) + 1) * (newInput.indexWhere(_ == div2) + 1) 
+  private val answer2: Int = (newInput.indexOf(div1) + 1) * (newInput.indexOf(div2) + 1)
   println(s"Answer day $day part 2: ${answer2} [${System.currentTimeMillis - start2}ms]")
 
