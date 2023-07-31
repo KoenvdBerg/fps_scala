18ccfd29fe91d7d34f9d79b56f47ad2f1efc7d63
Koen <kvandenberg@MacBook-Pro-van-Koen.local>
2023-07-31 14:42:18 +0200

    tremendous speed up for this puzzle

--- aoc2022/src/main/scala/day24.scala
+++ aoc2022/src/main/scala/day24.scala
@@ -26,23 +26,15 @@ import aoc2022.Grid2D.Point
  * the Elve would not be able to move to Point(2,2). In the above example, at all 4 points, no blizzard of the corresponding
  * type is present, and thus the Elve can move to Point(2,2). 
  * 
- * This can be computed with the blizzardLocs() function.  
+ * This can be computed with the doesntCollide() function.  
  * 
  * To track all the correct paths, I used a Set instead of a queue. This set holds all the current states, and each 
  * item in the set is used to compute the next state. Each iteration consists of all Elve locations and the 
  * corresponding time. 
  * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- *
  * PART 02:
  *
- * Sort and then take 3 and sum
+ * Used the implementation from part01 two more times, see below. 
  *
  */
 
@@ -55,84 +47,58 @@ object day24 extends App:
   private val start1: Long =
     System.currentTimeMillis
 
-  private val input: Vector[(Char, Point)] =
+  private val input: Vector[Vector[Char]] =
     
-    val infile = Source
+    Source
       .fromResource(s"day$day.txt")
       .getLines
-      .zipWithIndex
       .toVector
-
-    infile
-      .flatMap((ss: String, y: Int) => ss.zipWithIndex.map((cc: Char, x: Int) => (cc, Point(x, y))))
-      .filterNot(_._1 == '.')
-
+      .map(_.toVector.drop(1).dropRight(1))
+      .drop(1).dropRight(1)
+    
+    
   object Field:
-    val nextMoves: Map[Char, Point] = Map('>' -> Point(1, 0), '<' -> Point(-1, 0), 'v' -> Point(0, 1), 
-      '^' -> Point(0, -1), '#' -> Point(0, 0))
-    
-    def move(width: Int, height: Int)(blizzard: (Char, Point)): (Char, Point) = 
-      val nextLoc: Point = blizzard._2 + nextMoves(blizzard._1)
-      blizzard._1 match
-        case '>' if nextLoc.x == width  => ('>', Point(1, nextLoc.y))
-        case '<' if nextLoc.x == 0      => ('<', Point(width - 1, nextLoc.y))  
-        case 'v' if nextLoc.y == height => ('v', Point(nextLoc.x, 1))
-        case '^' if nextLoc.y == 0      => ('^', Point(nextLoc.x, height - 1))
-        case _                          => (blizzard._1, nextLoc)
-    
-    def blizzardStates(maxT: Int, startLocs: Vector[(Char, Point)], width: Int, height: Int): Map[Int, Vector[Point]] =
-      (1 to maxT).foldLeft((startLocs, Map(0 -> startLocs.map(_._2)))) { (res, t) =>
-        val updated = res._1.map(move(width, height))
-        (updated, res._2 + (t -> updated.map(_._2)))
-      }._2
-    
-    def blizzardPath(fieldStates: Map[Int, Vector[Point]], start: Point, finish: Point, startTime: Int): Int =
 
+    val width: Int = input.head.length
+    val height: Int = input.length
+    
+    def doesntCollide(field: Vector[Vector[Char]], loc: Point, time: Int): Boolean = 
+
+      def wrap(x: Int, rem: Int): Int =
+        val mod = x % rem
+        if mod < 0 then mod + rem else mod
+
+      loc.x >= 0 
+        && loc.x < width
+        && loc.y >= 0 
+        && loc.y < height
+        && field(loc.y)(wrap(loc.x - time, width)) != '>'
+        && field(loc.y)(wrap(loc.x + time, width)) != '<'
+        && field(wrap(loc.y - time, height))(loc.x) != 'v'
+        && field(wrap(loc.y + time, height))(loc.x) != '^'
+      
+    
+    def blizzardPath(field: Vector[Vector[Char]], start: Point, finish: Point, startTime: Int): Int =
+
+      @tailrec
       def go(active: Set[Point], time: Int): Int =
         val nextLocs: Set[Point] = active.flatMap(_.adjacentInclusive)
         if nextLocs(finish) then time
-        else if active.isEmpty then { println("ERROR, no path possible") ; time }
-        else if time >= 1000 then 9999
         else
-          val next: Set[Point] = nextLocs
-            .filter((p: Point) => p.x >= 0 && p.y >= 0 && !fieldStates(time).contains(p))
-          //println(s"time: $time --> $next")
-          go(next, time + 1)
+          val next: Set[Point] = nextLocs.filter((p: Point) => doesntCollide(field, p, time))
+          go(next + start, time + 1)  // add start in case waiting at start is needed
 
       go(Set(start), startTime)
       
-  // TODO: look at the code of JP
-
-  private val width: Int = input.maxBy(_._2.x)._2.x
-  private val height: Int = input.maxBy(_._2.y)._2.y
-  private val start: Point = Point(1, 0)
-  private val end: Point = Point(width - 1, height)
-  private val blizzards: Map[Int, Vector[Point]] = Field.blizzardStates(1000, input, width, height)
-  private val answer1: Int = Field.blizzardPath(blizzards, start, end, 0)
+  private val start: Point = Point(0, -1)
+  private val end: Point = Point(Field.width - 1, Field.height)
+  private val answer1: Int = Field.blizzardPath(input, start, end, 1)
   println(s"Answer day $day part 1: ${answer1} [${System.currentTimeMillis - start1}ms]")
-  
 
 
   private val start2: Long =
     System.currentTimeMillis
-    
-  private val res2: Int = Field.blizzardPath(blizzards, end, start, answer1)
-  private val answer2: Int = Field.blizzardPath(blizzards, start, end, res2)
+
+  private val res2: Int = Field.blizzardPath(input, end, start, answer1)
+  private val answer2: Int = Field.blizzardPath(input, start, end, res2)
   println(s"Answer day $day part 2: ${answer2} [${System.currentTimeMillis - start2}ms]")
-
-//def blizzardStates(maxT: Int, startLocs: Set[(Char, Point)], width: Int, height: Int): Map[Int, Set[(Char, Point)]] =
-//  (1 to maxT).foldLeft((startLocs, Map(0 -> startLocs.map(_._2)))) { (res, t) =>
-//    val updated = res._1.map(move(width, height))
-//    (updated, res._2 + (t -> updated.map(_._2)))
-//  }._2
-
-//val tests = blizzards.toVector.sortBy(_._1).map((_, x) => Point.gridPrintable(x.map(_._2).toVector)
-//((p: Point) =>
-//  if x.map(_._2)(p) then
-//    if x.count(_._2 == p) > 1 then s"${x.count(_._2 == p)}".head else x.filter(_._2 == p).head._1
-//  else '.'
-//))
-//tests.foreach(x =>
-//  println("-----")
-//  println(x)
-//)
