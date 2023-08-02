65a2f8bfc172e3c0b4acc5ec7210dec8f52ec235
Koen <kvandenberg@MBP-van-Koen.home>
2023-08-02 14:36:15 +0200

    finished aoc2021 day23

--- aoc2022/src/main/scala/day26.scala
+++ aoc2022/src/main/scala/day26.scala
@@ -1,21 +1,16 @@
 import scala.io.*
-import math.*
-import scala.annotation.tailrec
-import scala.collection.mutable
 import aoc2022.FlatGrid
 import aoc2022.VectorUtils.swap
-import aoc2022.Grid2D.Point
 import aoc2022.Algorithms.GraphTraversal.*
-import day26.Amphipod
 
 /**
  * PART 01:
  *
- * Easy foldRight
+ * Part 1 uses Dijkstra. It's not the fastest implementation, but gets the job done in about 5s. 
  *
  * PART 02:
  *
- * Sort and then take 3 and sum
+ * Uses A*. It gets the job done in about 50s. This could be greatly improved using a better heuristic. 
  *
  */
 
@@ -38,90 +33,132 @@ object day26 extends App:
     
   object Amphipod: 
   
-    // todo: for heuristic add option to ignore other amphipods perhaps...
-    def distanceGraph(ground: Vector[Char], width: Int): Graph[Int] =
+    def distanceGraph(ground: Vector[Char], width: Int, ignore: Boolean = false): Graph[Int] =
       (i: Int) =>
         val neighbours: Vector[Int] = FlatGrid.neighbours4(i, width, ground.length)
         if neighbours.isEmpty then Map.empty
         else
-          neighbours
-            .filter((n: Int) => ground(n) == '.')
-            .map((f: Int) => f -> 1)
-            .toMap
+          if ignore then
+            neighbours
+              .filter((n: Int) => ground(n) != '#')
+              .map((f: Int) => f -> 1)
+              .toMap            
+          else 
+            neighbours
+              .filter((n: Int) => ground(n) == '.')
+              .map((f: Int) => f -> 1)
+              .toMap
          
-    val hall: Vector[Int] = Vector(14, 15, 17, 19, 21, 23, 24) 
-    val room: Map[Char, Vector[Int]] = Map(
-      'A' -> Vector(29, 42), 
-      'B' -> Vector(31, 44), 
-      'C' -> Vector(33, 46), 
-      'D' -> Vector(35, 48))
-    val weights: Map[Char, Int] = Map(
+    private val hall: Vector[Int] = Vector(14, 15, 17, 19, 21, 23, 24) 
+    
+    private val room: Map[String, Map[Char, Vector[Int]]] = Map(
+      "part01" -> Map(
+        'A' -> Vector(29, 42),
+        'B' -> Vector(31, 44),
+        'C' -> Vector(33, 46),
+        'D' -> Vector(35, 48)),
+      "part02" -> Map(
+        'A' -> Vector(29, 42, 55, 68), 
+        'B' -> Vector(31, 44, 57, 70), 
+        'C' -> Vector(33, 46, 59, 72), 
+        'D' -> Vector(35, 48, 61, 74)))
+    
+    private val weights: Map[Char, Int] = Map(
       'A' -> 1,
       'B' -> 10, 
       'C' -> 100,
       'D' -> 1000)
+    
     val fin: Vector[Char] = Vector('#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '.', '.', '.', 
       '.', '.', '.', '.', '.', '.', '.', '.', '#', '#', '#', '#', 'A', '#', 'B', '#', 'C', '#', 'D', '#', '#', '#', ' ',
       ' ', '#', 'A', '#', 'B', '#', 'C', '#', 'D', '#', ' ', ' ', ' ', ' ', '#', '#', '#', '#', '#', '#', '#', '#', '#',
       ' ', ' ')
+
+    val fin2: Vector[Char] = Vector( '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '.', '.',
+      '.', '.', '.', '.', '.', '.', '.', '.', '.', '#', '#', '#', '#', 'A', '#', 'B', '#', 'C', '#', 'D', '#', '#', '#', 
+      ' ', ' ', '#', 'A', '#', 'B', '#', 'C', '#', 'D', '#', ' ', ' ', ' ', ' ', '#', 'A', '#', 'B', '#', 'C', '#', 'D', 
+      '#', ' ', ' ', ' ', ' ', '#', 'A', '#', 'B', '#', 'C', '#', 'D', '#', ' ', ' ', ' ', ' ', '#', '#', '#', '#', '#', 
+      '#', '#', '#', '#', ' ', ' ')
     
-    def amphipodGraph(width: Int): Graph[Vector[Char]] =
+    val insert: Vector[Char] = Vector(' ', ' ', '#', 'D', '#', 'C', '#', 'B', '#', 'A', '#', ' ', ' ', ' ', ' ', '#', 
+      'D', '#', 'B', '#', 'A', '#', 'C', '#', ' ', ' ')   
+    
+    def amphipodHeuristic(ground: Vector[Char]): Int =
+      val inHallway: Vector[Int] = hall.filter((i: Int) => ground(i) != '.')
+      val inPlace: Vector[Int] = room("part02")
+        .toVector
+        .flatMap((c: Char, ds: Vector[Int]) => ds.reverse.takeWhile((f: Int) => ground(f) == c))
+      val inRoom: Vector[Int] = room("part02").values.flatten.filter((i: Int) => ground(i) != '.' && !inPlace.contains(i)).toVector
+      val scores: Vector[Int] = for {
+        i <- inHallway ++ inRoom
+        dest <- room("part02")(ground(i)).lastOption
+        dist = shortestDistance(distanceGraph(ground, width, true))(i, dest)
+      } yield dist.get * weights(ground(i))
+      scores.sum
+
+    
+    def amphipodGraph(width: Int, part: String): Graph[Vector[Char]] =
       (ground: Vector[Char]) =>
-        // hallway into room
-        val inHallway: Vector[Int] = hall.filter((i: Int) => ground(i) != '.')
-        val hallwayMoves: Map[Vector[Char], Int] =
-          if inHallway.isEmpty then Map.empty
+        
+        // hallway into room (H2R)
+        val inHallway: Vector[Int] = hall.filter((i: Int) => ground(i) != '.')  // the amphipods in hallway that can move
+        val H2R: Map[Vector[Char], Int] =
+          if inHallway.isEmpty then Map.empty  // return no new nodes
           else 
             val ts: Vector[(Vector[Char], Int)] = for {
               i    <- inHallway
-              dest <- room(ground(i))
-                .filter((f: Int) => ground(f) == '.')
-                .lastOption
+              amphipod = ground(i)
+              destinations = room(part)(amphipod)
+              if destinations.forall((f: Int) => ground(f) == '.' || ground(f) == amphipod)  // target room may only contain empty or its own kind
+              dest <- destinations.filter((f: Int) => ground(f) == '.').lastOption
               dist = shortestDistance(distanceGraph(ground, width))(i, dest)
-              if dist != None
+              if dist != None  // make sure target destination is reachable
             } yield (swap(ground, i, dest), dist.get * weights(ground(i)))
             ts.toMap
         
-        // room into hallway
-        val inPlace: Vector[Int] = room
+        // room into hallway (R2H)
+        val inPlace: Vector[Int] = room(part)
           .toVector
           .flatMap((c: Char, ds: Vector[Int]) => ds.reverse.takeWhile((f: Int) => ground(f) == c))  // these amphipods are in the correct place already
         val inRoom: Vector[Int] = (for {
-          r    <- room.values
+          r    <- room(part).values
           todo <- r.find((i: Int) => ground(i) != '.')
           if !inPlace.contains(todo)  // don't move the amphipods from their correct place
         } yield todo).toVector
-        val roomMoves: Map[Vector[Char], Int] =
+        val R2H: Map[Vector[Char], Int] =
           if inRoom.isEmpty then Map.empty
           else 
             val rs = for {
               i    <- inRoom
               dest <- hall
               dist = shortestDistance(distanceGraph(ground, width))(i, dest)
-              if dist != None
+              if dist != None  // make sure target destination is reachable
             } yield (swap(ground, i, dest), dist.get * weights(ground(i)))
             rs.toMap
-        roomMoves ++ hallwayMoves
+        R2H ++ H2R
   
   end Amphipod
 
+
   // For animation of result uncomment code below
-  // private val res1: List[Vector[Char]] = shortestPath(Amphipod.amphipodGraph(width))(input, Amphipod.fin).get
-  // res1.foreach(f =>
-  //   Thread.sleep(500)
-  //   print("\u001b[2J")
-  //   println(FlatGrid.printFlatGrid(f, width)(identity))
-  // )
+//  private val res1 = shortestPath(Amphipod.amphipodGraph(width, "part01"))(input, Amphipod.fin, Amphipod.amphipodHeuristic("part01"))
+//  // For animation of result uncomment code below
+//   res1.get.foreach(f =>
+//     Thread.sleep(500)
+//     print("\u001b[2J")
+//     println(FlatGrid.printFlatGrid(f, width)(identity))
+//   )
 
-  // todo: part02
-  // todo: finish part01 by adding a heuristic
-  
-  private val answer1 = shortestDistance(Amphipod.amphipodGraph(width))(input, Amphipod.fin)
+
+  private val answer1 = shortestDistance(Amphipod.amphipodGraph(width, "part01"))(input, Amphipod.fin)
   println(s"Answer day $day part 1: ${answer1} [${System.currentTimeMillis - start1}ms]")
-
 
   private val start2: Long =
     System.currentTimeMillis
-    
-  private val answer2 = None
+  
+  private val input2: Vector[Char] =
+    val (left, rigtht) = input.splitAt(39)
+    left ++ Amphipod.insert ++ rigtht
+
+  private val answer2 = shortestDistance(Amphipod.amphipodGraph(width, "part02"))(input2, Amphipod.fin2, Amphipod.amphipodHeuristic)
   println(s"Answer day $day part 2: ${answer2} [${System.currentTimeMillis - start2}ms]")
