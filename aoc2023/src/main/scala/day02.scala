a949aeab9e7a3d94d4748abe75bc521917c9c0da
KoenvdBerg <koen-vandenberg@live.nl>
2023-12-02 06:39:08 +0100

    finished day02

--- aoc2023/src/main/scala/day02.scala
+++ aoc2023/src/main/scala/day02.scala
@@ -10,19 +10,48 @@ object day02 extends App:
   private val start1: Long =
     System.currentTimeMillis
 
-  private val input: List[String] =
+  case class Cube(color: String, n: Long)
+  case class Game(id: Int, cubes: Vector[Cube])
+
+  private val inputGames: Vector[Game] =
+
+    def parse(s: String): Game = s match
+      case s"Game $id: $tosplit" =>
+        val cubeSets: Seq[String] = tosplit.split(";")
+          .flatMap(_.strip().split(",").map(_.strip()))
+        val cubes: Vector[Cube] = cubeSets
+          .map { (c: String) =>
+            val cube1 = c.split(" ")
+            Cube(cube1.last, cube1.head.toLong)
+          }.toVector
+        Game(id.toInt, cubes)
+      case _ => sys.error("SLKDJFKLSDJf")
+
     Source
       .fromResource(s"day$day.txt")
       .getLines
-      .toList
+      .toVector
+      .map(parse)
 
+  private def validateGame(game: Game): Int =
+    val validated: Boolean = game.cubes.forall {
+      case Cube("green", n) => n <= 13
+      case Cube("red", n) => n <= 12
+      case Cube("blue", n) => n <= 14
+    }
+    if validated then game.id else 0
 
-  private val answer1 = ???
+  private def minCube(game: Game): Long =
+    game.cubes
+      .groupBy(_.color).values
+      .map(_.maxBy(_.n).n)
+      .product
+
+  private val answer1 = inputGames.map(validateGame).sum
   println(s"Answer day $day part 1: ${answer1} [${System.currentTimeMillis - start1}ms]")
-
 
   private val start2: Long =
     System.currentTimeMillis
 
-  private val answer2 = ???
+  private val answer2 = inputGames.map(minCube).sum
   println(s"Answer day $day part 2: ${answer2} [${System.currentTimeMillis - start2}ms]")
