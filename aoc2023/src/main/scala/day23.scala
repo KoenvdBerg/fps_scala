043126f891b5e96a56ea1f4b6f3ca4f13d07b990
KoenvdBerg <koen-vandenberg@live.nl>
2023-12-23 18:06:21 +0100

    finished and cleaned day 23

--- aoc2023/src/main/scala/day23.scala
+++ aoc2023/src/main/scala/day23.scala
@@ -1,18 +1,15 @@
-import aday23.pathFinder2
-
 import scala.io.*
 import math.*
 import scala.annotation.tailrec
 import scala.collection.immutable.Queue
 import scala.collection.mutable
 import aoc2022.Algorithms.GraphTraversal.Graph
-import aoc2022.Algorithms.GraphTraversal
 import aoc2022.Grid2D.Point
 
-object aday23 extends App:
+object day23 extends App:
 
   private val day: String =
-    this.getClass.getName.drop(4).init
+    this.getClass.getName.drop(3).init
 
   private val start1: Long =
     System.currentTimeMillis
@@ -24,8 +21,9 @@ object aday23 extends App:
       .toVector
 
   case class Path(x: Int, y: Int, prev: (Int, Int), distance: Int):
+
     def neighbours(curLoc: Char, part: Int = 1): Vector[Path] =
-      val ns = Vector(
+      val ns: Vector[Path] = Vector(
         (x + 1, y),
         (x - 1, y),
         (x, y - 1),
@@ -49,10 +47,11 @@ object aday23 extends App:
 
     def findPaths: Vector[Path] =
 
+      @tailrec
       def go(queue: Queue[Path], res: Vector[Path]): Vector[Path] =
         if queue.isEmpty then res
         else
-          val (p, nextq) = queue.dequeue
+          val (p: Path, nextq: Queue[Path]) = queue.dequeue
           if p.x -> p.y == target then go(nextq, res.appended(p))
           else
             val cur: Char = maze(p.y)(p.x)
@@ -64,7 +63,7 @@ object aday23 extends App:
 
 
     val cache: mutable.Map[Point, Vector[(Point, Int)]] = new mutable.HashMap()
-    def memWalkUntilWayPoint(from: Point) = cache.getOrElseUpdate(from, walkUntilWayPoint(from))
+    def memWalkUntilWayPoint(from: Point): Vector[(Point, Int)] = cache.getOrElseUpdate(from, walkUntilWayPoint(from))
     def walkUntilWayPoint(from: Point): Vector[(Point, Int)] =
 
       @tailrec
@@ -95,7 +94,7 @@ object aday23 extends App:
           .map((np, dist) => (np, visited.appended(p)) -> dist).toMap
 
 
-    def dijkstraMod(g: Graph[(Point, Vector[Point])])(source: Point, target: Point): Int =
+    def findLongestPath(g: Graph[(Point, Vector[Point])])(source: Point, target: Point): Int =
 
       val active: mutable.PriorityQueue[((Point, Vector[Point]), Int)] =
         mutable.PriorityQueue((source -> Vector.empty[Point], 0))(Ordering.by((f: ((Point, Vector[Point]), Int)) => f._2).reverse)
@@ -115,92 +114,17 @@ object aday23 extends App:
       go(0)
 
 
-
-
-
   private val pathFinder: PathFinder = PathFinder(input, 1)
-  private val res1 = pathFinder.findPaths
-  private val answer1 = res1.maxBy(_.distance).distance
+  private val res1: Vector[Path] = pathFinder.findPaths
+  private val answer1: Int = res1.maxBy(_.distance).distance
   println(s"Answer day $day part 1: ${answer1} [${System.currentTimeMillis - start1}ms]")
 
 
   private val start2: Long =
     System.currentTimeMillis
 
-
-  // too high: 6409
-  // test: 6230
-
   private val pathFinder2 = PathFinder(input, 2)
-
   private val start: Point = Point(pathFinder2.start.x, pathFinder2.start.y)
   private val target: Point = Point(pathFinder2.target._1, pathFinder2.target._2)
-  private val res2 = pathFinder2.dijkstraMod(pathFinder2.wayPointDAG)(start, target)
-  private val answer2 = res2
+  private val answer2 = pathFinder2.findLongestPath(pathFinder2.wayPointDAG)(start, target)
   println(s"Answer day $day part 2: ${answer2} [${System.currentTimeMillis - start2}ms]")
-
-
-/**
- * Answer day 23 part 1: 1930 [199ms]
- * Answer day 23 part 2: ((Point(105,37),Vector(Point(1,0), Point(17,11), Point(15,31), Point(9,59), Point(41,61),
- * Point(41,83), Point(5,83), Point(11,105), Point(39,129), Point(37,103), Point(63,109), Point(55,135), Point(79,137),
- * Point(83,103), Point(113,109), Point(107,135), Point(131,127), Point(129,111), Point(135,89), Point(101,77), Point(113,55),
- * Point(137,67), Point(127,29), Point(105,15), Point(87,19), Point(61,11), Point(29,9), Point(41,39), Point(55,33),
- * Point(55,57), Point(57,87), Point(89,85), Point(77,57), Point(79,29))),6409) [192307ms]
- */
-
-
-
-//
-//case class Walk(x: Int, y: Int, seen: Set[(Int, Int)], dist: Int)
-//
-//def findDistances: Vector[Int] =
-//
-//  def go(queue: Queue[Walk], res: Vector[Int]): Vector[Int] =
-//    if queue.isEmpty then res
-//    else
-//      val (w, nextq) = queue.dequeue
-//      if w.x -> w.y == target then go(nextq, res.appended(w.dist))
-//      else
-//        val next = wayPointsMap((w.x, w.y))
-//        val toEnqueue = next.filterNot((x, y, _) => w.seen.contains((x, y)))
-//          .map((x, y, d) => Walk(x, y, w.seen + ((w.x, w.y)), w.dist + d))
-//        go(nextq.enqueueAll(toEnqueue), res)
-//
-//  go(Queue(Walk(start.x, start.y, Set.empty, 0)), Vector.empty)
-//def waypointGraph: Graph[Path] =
-//  (p: Path) =>
-//    val np = p.copy(distance = 0)
-//    val next = np.neighbours(maze(np.y)(np.x), part)
-//    next
-//      .filter(pp => inBounds(pp.x, pp.y))
-//      .filter(pp => maze(pp.y)(pp.x) != '#')
-//      .map(pp => walkUntilWayPoint(pp))
-//      .map(pp => pp -> pp.distance)
-//      .toMap
-
-//    lazy val wayPoints: (Map[Path, Int], Map[Path, Path]) = GraphTraversal.dijkstra(waypointGraph)(start)
-//    lazy val wayPointsMap: Map[(Int, Int), Set[(Int, Int, Int)]] = wayPoints._2.groupBy(_._2).map((k, v) => (k, v.keySet))
-//      .map((k, v) => (k.x, k.y) -> v.map(p => (p.x, p.y, p.distance)))io
-
-//    def wayPointMap: Map[(Point, Point), Int] =
-//
-////      val seen = mutable.Set[(Point, Point)]()
-////          seen.addAll(next.map(p => (p.x, p.y)))
-//      //          val nextRes = cur.foldLeft(res) { (r, p) =>
-//      //            r.updated((p.x, p.y), next.map(pp => (pp.x, pp.y, pp.distance)))
-//      //          }
-//
-//
-//      def go(cur: Set[Point], res: Map[(Point, Point), Int]): Map[(Point, Point), Int] =
-//        if cur.isEmpty then res
-//        else
-//          val next = cur.map(c =>
-//            val (found, dist) = walkUntilWayPoint(c)
-//            (c, found) -> dist)
-//            .filterNot(pp => res.contains((pp._1._1, pp._1._2)) | res.contains(pp._1._2, pp._1._1))
-//          println(s"$cur --> $next")
-//          val nextRes = res ++ next.toMap
-//          go(next.map(_._1._2), nextRes)
-//
-//      go(Set(Point(start.x, start.y)), Map.empty)
