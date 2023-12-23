38d70d8ba80ff0e43ec2169031fda9260463c79b
KoenvdBerg <koen-vandenberg@live.nl>
2023-12-23 12:01:25 +0100

    finished part01, working on part02

--- aoc2023/src/main/scala/day23.scala
+++ aoc2023/src/main/scala/day23.scala
@@ -1,28 +1,107 @@
 import scala.io.*
 import math.*
 import scala.annotation.tailrec
+import scala.collection.immutable.Queue
+import aoc2022.Algorithms.GraphTraversal.Graph
+import aoc2022.Algorithms.GraphTraversal
 
-object day23 extends App:
+object aday23 extends App:
 
   private val day: String =
-    this.getClass.getName.drop(3).init
+    this.getClass.getName.drop(4).init
 
   private val start1: Long =
     System.currentTimeMillis
 
-  private val input: List[String] =
+  private val input: Vector[String] =
     Source
       .fromResource(s"day$day.txt")
       .getLines
-      .toList
+      .toVector
+
+  class PathFinder(maze: Vector[String], part: Int):
+    private val maxX: Int = maze.head.length
+    private val maxY: Int = maze.length
+    val start: Path = Path(1, 0, (1, -1), 0)
+    val target: (Int, Int) = (maxX - 2, maxY - 1)
+
+    def inBounds(x: Int, y: Int): Boolean = x >= 0 && x < maxX && y >= 0 && y < maxY
+
+    case class Path(x: Int, y: Int, prev: (Int, Int), distance: Int):
+      def neighbours(curLoc: Char): Vector[Path] =
+        val ns = Vector(
+          (x + 1, y),
+          (x - 1, y),
+          (x, y - 1),
+          (x, y + 1)
+        ).map((xx, yy) => Path(xx, yy, (x, y), distance + 1))
+        val logicalNext: Vector[Path] = curLoc match
+          case '>' if part == 1 => ns.take(1)
+          case 'v' if part == 1 => ns.takeRight(1)
+          case _   => ns
+
+        logicalNext.filter(pp => (pp.x, pp.y) != prev)
+
+    def findPaths: Vector[Path] =
+
+      def go(queue: Queue[Path], res: Vector[Path]): Vector[Path] =
+        if queue.isEmpty then res
+        else
+          val (p, nextq) = queue.dequeue
+          if p.x -> p.y == target then go(nextq, res.appended(p))
+          else
+            val cur: Char = maze(p.y)(p.x)
+            val ns: Vector[Path] = p.neighbours(cur)
+              .filter(pp => maze(pp.y)(pp.x) != '#' && inBounds(pp.x, pp.y))
+            go(nextq.enqueueAll(ns), res)
+
+      go(Queue(start), Vector.empty)
 
 
-  private val answer1 = ???
+    @tailrec
+    final def walkUntilWayPoint(p: Path): (Path, Int) =
+      if p.x -> p.y == target then p -> p.distance
+      else if p.x -> p.y == (start.x, start.y) then p -> p.distance
+      else
+        val cur: Char = maze(p.y)(p.x)
+        val ns: Vector[Path] = p.neighbours(cur)
+          .filter(pp => maze(pp.y)(pp.x) != '#' && inBounds(pp.x, pp.y))
+        if ns.length >= 2 then p -> p.distance
+        else walkUntilWayPoint(ns.head)
+
+    def mazeGraph: Graph[(Int, Int)] =
+      (x: Int, y: Int) =>
+        val np = Path(x, y, (-1, -1), 0)
+        val next = np.neighbours(maze(np.y)(np.x))
+        next
+          .filter(pp => inBounds(pp.x, pp.y))
+          .filter(pp => maze(pp.y)(pp.x) != '#')
+          .map(pp => walkUntilWayPoint(pp))
+          .map(pp => (pp._1.x, pp._1.y) -> pp._2)
+          .toMap
+
+
+
+
+
+
+  private val pathFinder: PathFinder = PathFinder(input, 1)
+  private val res1 = pathFinder.findPaths
+  private val answer1 = res1.maxBy(_.distance).distance
   println(s"Answer day $day part 1: ${answer1} [${System.currentTimeMillis - start1}ms]")
 
 
   private val start2: Long =
     System.currentTimeMillis
 
-  private val answer2 = ???
-  println(s"Answer day $day part 2: ${answer2} [${System.currentTimeMillis - start2}ms]")
+
+  val pathFinder2 = PathFinder(input, 2)
+  private val res2 = GraphTraversal.dijkstra(pathFinder2.mazeGraph)((pathFinder2.start.x, pathFinder2.start.y))
+
+
+  println(res2._2.toVector.mkString("\n"))
+  println()
+  println(res2._1.toVector.mkString("\n"))
+
+//  private val answer2 = res2.maxBy(_.distance).distance
+//  println(s"Answer day $day part 2: ${answer2} [${System.currentTimeMillis - start2}ms]")
