4e7764bac1b2e6166ca02f2c9ac070379b4a2ae5
KoenvdBerg <koen-vandenberg@live.nl>
2024-03-30 16:24:07 +0100

    working

--- ai_book/src/main/scala/ai/paip/GPS.scala
+++ ai_book/src/main/scala/ai/paip/GPS.scala
@@ -3,19 +3,23 @@ package ai.paip
 import ai.paip.SchoolStates.*
 import ai.paip.SchoolActions.*
 
+import scala.collection.mutable.ListBuffer
+
 // https://github.com/norvig/paip-lisp/tree/main
 
 case class Op(action: Action, precons: List[State], addList: List[State], delList: List[State] = Nil):
 
   def isAppropriate(goal: State): Boolean = addList.contains(goal)
 
-case class Tally(state: Set[State], done: Vector[State])
 
 class GPS(ops: Vector[Op]):
 
-  def gps(state: Set[State], goals: List[State]): Set[State] =
-    // add remove-if #'atom
+  private val done: ListBuffer[Action] = ListBuffer.empty
+
+  def run(state: Set[State], goals: List[State]): Vector[Action] =
+    done.clear()
     achieveAll(state + Start, goals, List.empty[State])
+    done.toVector
 
   def achieveAll(state: Set[State], goals: List[State], goalStack: List[State]): Set[State] =
     val thisState: Set[State] = goals.flatMap(g => achieve(state, g, goalStack)).toSet
@@ -30,7 +34,7 @@ class GPS(ops: Vector[Op]):
   def applyGoal(op: Op, state: Set[State], goal: State, goalStack: List[State]): Set[State] =
     val nextState = achieveAll(state, op.precons, goal :: goalStack)
     if nextState.nonEmpty then
-      println(s"doing: ${op.action}")
+      done.addOne(op.action)
       state.diff(op.delList.toSet) ++ op.addList.toSet
     else nextState
 
@@ -40,12 +44,12 @@ trait Action
 case object Start extends State
 
 enum SchoolActions extends Action:
-  case DriveSonToSchool(done: Boolean = false)
-  case GiveShopMoney(done: Boolean = false)
-  case TelephoneShop(done: Boolean = false)
-  case LookUpPhoneNumber(done: Boolean = false)
-  case TellShopProblem(done: Boolean = false)
-  case ShopInstallsBattery(done: Boolean = false)
+  case DriveSonToSchool
+  case GiveShopMoney
+  case TelephoneShop
+  case LookUpPhoneNumber
+  case TellShopProblem
+  case ShopInstallsBattery
 
 
 enum SchoolStates extends State:
@@ -53,14 +57,6 @@ enum SchoolStates extends State:
   case HavePhoneBook
   case HaveMoney
   case KnowPhoneNumber
-
-  // actions:
-//  case DriveSonToSchool
-//  case GiveShopMoney
-//  case TelephoneShop
-//  case LookUpPhoneNumber
-//  case TellShopProblem
-
 
   // son states:
   case SonAtSchool
@@ -72,7 +68,6 @@ enum SchoolStates extends State:
 
   // carshop states:
   case ShopKnowsProblem
-//  case ShopInstallsBattery
   case InCommunicationWithShop
   case ShopHasMoney
 
@@ -80,20 +75,21 @@ enum SchoolStates extends State:
 object SchoolStates:
 
   val schoolOps: Vector[Op] = Vector(
-    Op(DriveSonToSchool(), List(SonAtHome, CarWorks), List(SonAtSchool), List(SonAtHome)),
-    Op(ShopInstallsBattery(), List(CarNeedsBattery, ShopKnowsProblem, ShopHasMoney), List(CarWorks)),
-    Op(TellShopProblem(), List(InCommunicationWithShop), List(ShopKnowsProblem)),
-    Op(TelephoneShop(), List(KnowPhoneNumber), List(InCommunicationWithShop)),
-    Op(LookUpPhoneNumber(), List(HavePhoneBook), List(KnowPhoneNumber)),
-    Op(GiveShopMoney(), List(HaveMoney), List(ShopHasMoney), List(HaveMoney))
+    Op(DriveSonToSchool, List(SonAtHome, CarWorks), List(SonAtSchool), List(SonAtHome)),
+    Op(ShopInstallsBattery, List(CarNeedsBattery, ShopKnowsProblem, ShopHasMoney), List(CarWorks)),
+    Op(TellShopProblem, List(InCommunicationWithShop), List(ShopKnowsProblem)),
+    Op(TelephoneShop, List(KnowPhoneNumber), List(InCommunicationWithShop)),
+    Op(LookUpPhoneNumber, List(HavePhoneBook), List(KnowPhoneNumber)),
+    Op(GiveShopMoney, List(HaveMoney), List(ShopHasMoney), List(HaveMoney))
   )
 
 
 
 @main def test_gps: Unit =
 
-  val gps = GPS(SchoolStates.schoolOps).gps(Set(SonAtHome, CarNeedsBattery, HaveMoney, HavePhoneBook), List(SonAtSchool))
-  println(gps)
+  val gps = GPS(SchoolStates.schoolOps)
+  val result = gps.run(Set(SonAtHome, CarNeedsBattery, HaveMoney, HavePhoneBook), List(SonAtSchool))
+  println(s"Having done: $result")
 
 
 
