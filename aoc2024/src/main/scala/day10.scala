2f5139b3e306d89cab387de489c660e80e7acb39
Koen <kvandenberg@MacBook-Pro-van-Koen.local>
2024-12-10 08:51:55 +0100

    fin day10

--- aoc2024/src/main/scala/day10.scala
+++ aoc2024/src/main/scala/day10.scala
@@ -7,7 +7,7 @@ import scala.annotation.tailrec
 import scala.collection.immutable.Queue
 import scala.util.matching.Regex
 
-object day00 extends App:
+object day10 extends App:
 
   private val day: String = "10"
 
@@ -40,21 +40,32 @@ object day00 extends App:
         algorithm(nq, grid, acc + 1)
       else
         val next = step(grid, n)
-        algorithm(nq.enqueueAll(next), grid, acc)
+        algorithm(nq.enqueueAll(next).distinct, grid, acc)
 
   def algorithmAt(p: (Int, Int), grid: BoundedGrid): Int =
     algorithm(Queue(p), grid, 0)
 
-  val starts = input.
-
-
-  private val answer1 = algorithmAt((3, 0), input)
+  private val starts = input.getPointsOf('0')
+  private val answer1 = starts.map(s => algorithmAt(s, input)).sum
   println(s"Answer day $day part 1: ${answer1} [${System.currentTimeMillis - start1}ms]")
 
   private val start2: Long =
     System.currentTimeMillis
 
-  private val answer2 = ""
+  def algorithm2(q: Queue[(Int, Int)], grid: BoundedGrid, acc: Int): Int =
+    if q.isEmpty then acc
+    else
+      val (n, nq) = q.dequeue
+      if grid.grid(n._2)(n._1) == '9' then
+        algorithm2(nq, grid, acc + 1)
+      else
+        val next = step(grid, n)
+        algorithm2(nq.enqueueAll(next), grid, acc)
+
+  def algorithmAt2(p: (Int, Int), grid: BoundedGrid): Int =
+    algorithm2(Queue(p), grid, 0)
+
+  private val answer2 = starts.map(s => algorithmAt2(s, input)).sum
   println(s"Answer day $day part 2: ${answer2} [${System.currentTimeMillis - start2}ms]")
 
 
